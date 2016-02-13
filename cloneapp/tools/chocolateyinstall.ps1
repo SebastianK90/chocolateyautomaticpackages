9b38d88fe17ecccb727c129a4a998f4e9f52bcc7
sippi90 <kowatsch.sebastian@outlook.de>
2016-02-13 20:25:07 +0100

    .

--- cloneapp/tools/chocolateyinstall.ps1
+++ cloneapp/tools/chocolateyinstall.ps1
@@ -9,3 +9,28 @@
   Install-ChocolateyShortcut -shortcutFilePath "$env:USERPROFILE\Desktop\CloneApp.lnk" $FileFullPath
  
   Install-ChocolateyPath $toolsDir
+  
+  if(Test-Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers')
+    {
+	Sleep 1
+      New-ItemProperty 'HKCU:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers' -Name $FileFullPath.FullName -Value 'RUNASADMIN' -PropertyType 'String' -Force
+    }
+    else
+    {
+	Sleep 1
+      New-Item 'HKCU:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers' -Force
+      New-ItemProperty 'HKCU:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers' -Name $FileFullPath.FullName -Value 'RUNASADMIN' -PropertyType 'String' -Force
+	}
+
+if ((Get-WmiObject win32_operatingsystem).caption -like '*Server*')
+{
+  if(Test-Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers')
+    {
+	 New-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers' -Name $FileFullPath.FullName -Value 'DisableNXShowUI' -PropertyType 'String' -Force
+    }
+    else
+    {
+	 New-Item 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers' -Force
+	 New-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers' -Name $FileFullPath.FullName -Value 'DisableNXShowUI' -PropertyType 'String' -Force
+	}
+}
