df29ab6bc8ee0fc50828d343f3c0c99af9acb856
sippi90 <kowatsch.sebastian@outlook.de>
2016-02-20 04:20:09 +0100

    .

--- copyfilename/tools/chocolateyInstall.ps1
+++ copyfilename/tools/chocolateyInstall.ps1
@@ -1,11 +1,6 @@
 ﻿$packageName = 'copyfilename'
-$installerType = 'EXE'
-$silentArgs = '/VERYSILENT'
-$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$installerType = 'exe'
 $url = '{{DownloadUrl}}'
-Install-ChocolateyZipPackage "$packageName" "$url" "$toolsDir"
+$silentArgs = '/VERYSILENT'
  
-$FileFullPath = get-childitem $toolsDir -recurse -include *.exe | select -First 1
-    
-Install-ChocolateyInstallPackage "$packageName" "$installerType" "$silentArgs" "$FileFullPath"
-Remove-Item "$FileFullPath"
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"
