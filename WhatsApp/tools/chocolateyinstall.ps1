732a38ebc603f370225df7dde93c098e13303b5e
sippi90 <kowatsch.sebastian@outlook.de>
2016-05-13 19:17:08 +0200

    .

---
+++ WhatsApp/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿  $packageName= 'WhatsApp'
+  $toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+  $installerType = 'EXE'
+  $url        =  'https://web.whatsapp.com/desktop/windows/release/ia32/WhatsAppSetup.exe'
+  $url64 = 'https://web.whatsapp.com/desktop/windows/release/x64/WhatsAppSetup.exe'
+  Install-ChocolateyPackage $packageName $fileType $silentArgs $url $url64
+  
+  
