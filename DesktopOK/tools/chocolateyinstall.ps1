14421fcae2decdf330237d6ed7832abf0e8883ad
sippi90 <kowatsch.sebastian@outlook.de>
2016-05-18 17:02:00 +0200

    #upgrade Fix
    requires chocolatey 0.9.9.10+

--- DesktopOK/tools/chocolateyinstall.ps1
+++ DesktopOK/tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
-﻿  $packageName= 'desktopok'
-  $toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-  $url        =  '{{DownloadUrl}}'
-  $url64      =  '{{DownloadUrlx64}}'
+﻿$packageName= 'desktopok'
+$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$url        =  '{{DownloadUrl}}'
+$url64      =  '{{DownloadUrlx64}}'
 
-  Install-ChocolateyZipPackage "$packageName" "$url" "$toolsDir" "$url64"
+Install-ChocolateyZipPackage -PackageName "$packageName" -url "$url" "$toolsDir" -url64 "$url64"
   
-  $FileFullPath = get-childitem $toolsDir -recurse -include *.exe | select -First 1
+$FileFullPath = get-childitem $toolsDir -recurse -include *.exe | select -First 1
     
-  Install-ChocolateyShortcut -shortcutFilePath "$env:USERPROFILE\Desktop\DesktopOK.lnk" $FileFullPath
+Install-ChocolateyShortcut -shortcutFilePath "$env:USERPROFILE\Desktop\DesktopOK.lnk" $FileFullPath
  
---
+++ ECM_unfertig/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$packageName= 'ecm'
+$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$url        =  '{{DownloadUrl}}'
+$url64      =  '{{DownloadUrlx64}}'
+ 
+Install-ChocolateyZipPackage -PackageName "$packageName" -url "$url" "$toolsDir" -url64 "$url64"
+ 
+ if (Get-ProcessorBits -eq '64')
+     
+        {
+        $FileFullPath = get-childitem $toolsDir -recurse -include EcMenu_x64.exe | select -First 1
+        }
+    else 
+        {
+        $FileFullPath = get-childitem $toolsDir -recurse -include EcMenu.exe | select -First 1
+        }
+    
+    
+Install-ChocolateyShortcut -shortcutFilePath "$env:USERPROFILE\Desktop\Easy Context Menu.lnk" $FileFullPath
