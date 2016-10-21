f096589327895a1b3f0ed0c2450163b22a386264
sippi90 <kowatsch.sebastian@outlook.de>
2016-10-21 18:21:27 +0200

    .

--- CoreTemp/tools/chocolateyuninstall.ps1
+++
@@ -1,3 +0,0 @@
-﻿Get-Process "Core Temp*" | Stop-Process -Force -ErrorAction SilentlyContinue
-$icon_name = (Get-ChildItem "$env:ALLUSERSPROFILE\Microsoft\Windows\Start Menu\Programs" -Filter "Core Temp.lnk" -ErrorAction SilentlyContinue).FullName
-Remove-Item $icon_name -ErrorAction SilentlyContinue
