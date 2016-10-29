c10b6bbbac91e85c215bedd8283c926e61ed0d1f
sippi90 <kowatsch.sebastian@outlook.de>
2016-10-29 23:15:57 +0200

    switched to Uninstall-ChocolateyPackage

--- gpmdp/tools/chocolateyUninstall.ps1
+++ gpmdp/tools/chocolateyUninstall.ps1
@@ -1,14 +1,1 @@
-﻿$uninstall = Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall, HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall, HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall |
-    Get-ItemProperty |
-        Where-Object {$_.DisplayName -like "*Google Play Music Desktop Player*" } |
-            Select-Object -Property DisplayName, UninstallString
- 
-ForEach ($ver in $uninstall) {
- 
-    If ($ver.UninstallString) {
- 
-        $uninst = $ver.UninstallString
-        & cmd /c $uninst --uninstall
-    }
- 
-}
+﻿Uninstall-ChocolateyPackage -PackageName 'gpmdp' -FileType 'EXE' -SilentArgs '--uninstall'  -File "$env:LOCALAPPDATA\GPMDP_3\Update.exe"
