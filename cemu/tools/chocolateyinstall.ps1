83bf6801711b5ae3317ad0ffb2d7337bf22ed0d3
sippi90 <kowatsch.sebastian@outlook.de>
2016-11-03 15:47:33 +0100

    init cemu

---
+++ cemu/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = 'cemu'
+$url64       = 'http://cemu.info/releases/cemu_1.6.1.zip'
+$checksum64  = 'faca4836086c9831894e5099ab1260d256dadde5a28719a1fd4f9897945ae334'
+$toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  packageName    = $packageName
+  url            = $url64
+  checksum       = $checksum64
+  checksumType   = 'sha256'
+  unzipLocation  = $toolsPath
+}
+Install-ChocolateyZipPackage @packageArgs
+
+$FileFullPath = get-childitem $toolsPath -recurse -include *.exe | select -First 1
+  
+Install-ChocolateyShortcut -shortcutFilePath "$env:ALLUSERSPROFILE\Microsoft\Windows\Start Menu\Programs\CEMU.lnk" $FileFullPath
