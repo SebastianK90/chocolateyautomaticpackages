ddcad42645713ac0537b5550a81e09a4fc9be740
sippi90 <kowatsch.sebastian@outlook.de>
2016-11-04 02:03:46 +0100

    fixed Uninstall-ChocolateyPackage

--- gpmdp/tools/chocolateyUninstall.ps1
+++ gpmdp/tools/chocolateyUninstall.ps1
@@ -1,1 +1,1 @@
-﻿Uninstall-ChocolateyPackage -PackageName 'gpmdp' -FileType 'EXE' -SilentArgs '--uninstall'  -File "$env:LOCALAPPDATA\GPMDP_3\Update.exe"
+﻿Uninstall-ChocolateyPackage -PackageName 'gpmdp' -FileType 'EXE' -SilentArgs ' --uninstall'  -File "$env:LOCALAPPDATA\GPMDP_3\Update.exe"
