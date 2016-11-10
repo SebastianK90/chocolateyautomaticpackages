6dbf98383d3e68551b133caf92ba9679238d5ac2
sippi90 <kowatsch.sebastian@outlook.de>
2016-11-10 18:44:52 +0100

    Update

--- update_all.ps1
+++ update_all.ps1
@@ -1,16 +1,12 @@
 # AU Packages Template: https://github.com/majkinetor/au-packages-template
 
-param([string] $Name, [string] $ForcedPackages, [string] $Root = $PSScriptRoot)
-
-
-Set-ItemProperty 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3'-Name 1A10 -Value 0 -Force
-
+param([string] $Name, [string] $ForcedPackages, [string] $Root = "$PSScriptRoot\automatic")
 
 if (Test-Path $PSScriptRoot/update_vars.ps1) { . $PSScriptRoot/update_vars.ps1 }
 
 $Options = [ordered]@{
     Timeout       = 100                                     #Connection timeout in seconds
-    UpdateTimeout = 780                                    #Update timeout in seconds
+    UpdateTimeout = 1200                                    #Update timeout in seconds
     Threads       = 10                                      #Number of background jobs to use
     Push          = $Env:au_Push -eq 'true'                 #Push to chocolatey
     PluginPath    = ''                                      #Path to user plugins
@@ -21,14 +17,22 @@ $Options = [ordered]@{
         Params= @{                                          #Report parameters:
             Github_UserRepo = $Env:github_user_repo         #  Markdown: shows user info in upper right corner
             NoAppVeyor  = $false                            #  Markdown: do not show AppVeyor build shield
-            UserMessage = ''                                #  Markdown, Text: Custom user message to show
+            UserMessage = "[History](#update-history) | **USING AU NEXT VERSION**"       #  Markdown, Text: Custom user message to show
+            NoIcons     = $false                            #  Markdown: don't show icon
+            IconSize    = 32                                #  Markdown: icon size
         }
     }
 
+    History = @{
+        Lines = 30                                          #Number of lines to show
+        Github_UserRepo = $Env:github_user_repo             #User repo to be link to commits
+        Path = "$PSScriptRoot\UpdateHistory.md"             #Path where to save history
+    }
+
     Gist = @{
-        Id          = $Env:gist_id                          #Your gist id or leave empty for anonymous
-        ApiKey      = $Env:github_api_key                   #Your github api key
-        Path        = "$PSScriptRoot\Update-AUPackages.md"  #List of files to add to gist
+        Id     = $Env:gist_id                               #Your gist id; leave empty for new private or anonymous gist
+        ApiKey = $Env:github_api_key                        #Your github api key - if empty anoymous gist is created
+        Path   = "$PSScriptRoot\Update-AUPackages.md", "$PSScriptRoot\UpdateHistory.md"       #List of files to add to the gist
     }
 
     Git = @{
@@ -58,16 +62,18 @@ $Options = [ordered]@{
     ForcedPackages = $ForcedPackages -split ' '
     BeforeEach = {
         param($PackageName, $Options )
-        if ($Options.ForcedPackages -contains $PackageName) { $global:au_Force = $true }
+        $p = $Options.ForcedPackages | ? { $_ -match "^${PackageName}(?:\:(.+))*$" }
+        if (!$p) { return }
+
+        $global:au_Force   = $true
+        $global:au_Version = ($p -split ':')[1]
     }
 }
 
 
 if ($ForcedPackages) { Write-Host "FORCED PACKAGES:  $ForcedPackages" }
 $global:au_Root = $Root                                    #Path to the AU packages
-$info = updateall -Name $Name -Options $Options
+$global:info = updateall -Name $Name -Options $Options
 
 #Uncomment to fail the build on AppVeyor on any package error
-#if ($info.error_count.total) { throw 'Errors during update' }
-
-Set-ItemProperty 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3'-Name 1A10 -Value 1 -Force
+#if ($global:info.error_count.total) { throw 'Errors during update' }
