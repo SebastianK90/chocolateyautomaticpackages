5e0751e5b1c6768c5fa2faeb3f574a0c0feffdba
sippi90 <kowatsch.sebastian@outlook.de>
2016-11-10 18:24:38 +0100

    github .nuspec update

--- update_all.ps1
+++ update_all.ps1
@@ -31,10 +31,10 @@ $Options = [ordered]@{
         Path        = "$PSScriptRoot\Update-AUPackages.md"  #List of files to add to gist
     }
 
-    #Git = @{
+    Git = @{
         #User     = ''                                       #Git username, leave empty if github api key is used
-        #Password = $Env:github_api_key                      #Password if username is not empty, otherwise api key
-    #}
+        Password = $Env:github_api_key                      #Password if username is not empty, otherwise api key
+    }
 
     RunInfo = @{
         Exclude = 'password', 'apikey'                      #Option keys which contain those words will be removed
