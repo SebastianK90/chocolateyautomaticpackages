084dccc9ab31a8100ce2d12a1566be138ef39c62
sippi90 <kowatsch.sebastian@outlook.de>
2016-11-15 17:20:59 +0100

    Test

--- WhatsApp/update.ps1
+++ WhatsApp/update.ps1
@@ -16,11 +16,19 @@ function global:au_SearchReplace {
 
 function global:au_GetLatest {
     #$download_page = Invoke-WebRequest -Uri $releases
-
+    $useragents = @('Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko',`
+    'Mozilla/5.0 (X11; Linux x86_64; rv:17.0) Gecko/20121202 Firefox/17.0 Iceweasel/17.0.1',`
+    'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1',`
+    'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',`
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',`
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2750.0 Iron Safari/537.36'
+)
+    $random = Get-Random -Maximum 6
+    
     $url64   = 'https://web.whatsapp.com/desktop/windows/release/x64/WhatsAppSetup.exe'
     $url32   = 'https://web.whatsapp.com/desktop/windows/release/ia32/WhatsAppSetup.exe'
     $output = "$env:TEMP\WhatsAppSetup.exe"
-    Invoke-WebRequest -Uri $url32 -OutFile $output
+    Invoke-WebRequest -Uri $url32 -OutFile $output -UserAgent [$useragents[$random]]
     $version = ((Get-ChildItem $output).VersionInfo).fileversion
     Remove-Item $output
     #$version = [regex]::match(($download_page.Links.innerhtml | Where-Object {$_ -like '*<span>Whatsapp*'}),'[0-9]+(\.[0-9]+)*').value
