1d2aafcb14bd0ec966dbf58309216c290a2725f5
sippi90 <kowatsch.sebastian@outlook.de>
2016-11-21 17:31:27 +0100

    Saving Double Driver from Extinction
    did various downloads from different sites.
    all checksums match:
    http://pastebin.com/n4tYq4LR

--- doubledriver/tools/chocolateyinstall.ps1
+++ doubledriver/tools/chocolateyinstall.ps1
@@ -1,7 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'doubledriver'
-$toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
+$toolsPath   = (Split-Path -parent $MyInvocation.MyCommand.Definition)
+
+
+Get-ChocolateyUnzip -FileFullPath "$toolsPath\double_driver_4.1.0_portable.zip" -Destination $toolsPath
+
 
 $FileFullPath = get-childitem $toolsPath -recurse -include dd.exe | select -First 1
   
