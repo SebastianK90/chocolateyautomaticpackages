49a4166c3fb8f2260f544375e1b612c05913a13e
sippi90 <kowatsch.sebastian@outlook.de>
2016-12-05 17:47:04 +0100

    Update

--- GPMDP/tools/chocolateyInstall.ps1
+++ GPMDP/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 $ErrorActionPreference = 'Stop'
 
 $packageName = 'gpmdp'
-$url32       = 'https://github.com/MarshallOfSound/Google-Play-Music-Desktop-Player-UNOFFICIAL-/releases/download/v4.0.3/Google.Play.Music.Desktop.PlayerSetup.exe'
-$checksum32  = '6ad7f511d45a4020ef4e2c3c722c4c581709b81bc417f8cc40e9fd62a4ee6573'
+$url32       = 'https://github.com/MarshallOfSound/Google-Play-Music-Desktop-Player-UNOFFICIAL-/releases/download/v4.0.4/Google.Play.Music.Desktop.Player.Setup.exe'
+$checksum32  = '316b8a12b0b7244ec169c4b2621249218946eacb0628a8b6bb50b4c21cb756b7'
 
 $packageArgs = @{
   packageName            = $packageName
