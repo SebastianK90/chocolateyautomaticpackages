95ed1d3664773289cf97d6da5f589da679ff87fc
sippi90 <kowatsch.sebastian@outlook.de>
2017-02-17 18:13:48 +0100

    keep settings & license after update

--- aida64-extreme.portable/tools/chocolateyInstall.ps1
+++ aida64-extreme.portable/tools/chocolateyInstall.ps1
@@ -4,6 +4,7 @@ $packageName = 'aida64-extreme.portable'
 $url32       = 'http://download.aida64.com/aida64extreme580.zip'
 $checksum32  = '58e6c100f886d82f6892baf5e6c5de8fade96839750f4473d8d7f592519e5932'
 $toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
+$aida64_temp = "$env:USERPROFILE\aida64_temp"
 
 $packageArgs = @{
   packageName    = $packageName
@@ -12,8 +13,14 @@ $packageArgs = @{
   checksumType   = 'sha256'
   unzipLocation  = $toolsPath
 }
+
 Install-ChocolateyZipPackage @packageArgs
 
 $FileFullPath = get-childitem $toolsPath -recurse -include aida64.exe | select -First 1
   
 Install-ChocolateyShortcut -shortcutFilePath "$env:ALLUSERSPROFILE\Microsoft\Windows\Start Menu\Programs\AIDA64.lnk" $FileFullPath
+
+if(Test-Path $aida64_temp)
+    {
+    Move-Item (Get-ChildItem $aida64_temp) -Destination $toolsPath -Force
+    }
---
+++ aida64-extreme.portable/tools/chocolateybeforemodify.ps1
@@ -0,0 +1,8 @@
+﻿Get-Process "AIDA64*" | Stop-Process -Force -ErrorAction SilentlyContinue
+$settings_lic = Get-Item "$toolsPath\pkey.txt","$toolsPath\aida64.ini" -ErrorAction SilentlyContinue
+$aida64_temp = "$env:USERPROFILE\aida64_temp"
+
+if($settings_lic -ne $null)
+    {
+    Move-Item "$settings_lic" $aida64_temp -Force
+    }
