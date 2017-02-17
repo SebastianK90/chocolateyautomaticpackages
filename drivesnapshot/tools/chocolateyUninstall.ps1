97aa830614bde539cbb7106fc03388b3b43522cb
sippi90 <kowatsch.sebastian@outlook.de>
2017-02-17 17:12:30 +0100

    integrated license support

--- drivesnapshot/tools/chocolateyUninstall.ps1
+++ drivesnapshot/tools/chocolateyUninstall.ps1
@@ -1,2 +1,9 @@
+$lic_path = "$env:USERPROFILE\drivesnapshot_lic"
 $icon_name = (Get-ChildItem "$env:ALLUSERSPROFILE\Microsoft\Windows\Start Menu\Programs" -Filter "Drive Snapshot.lnk" -ErrorAction SilentlyContinue).FullName
+
+if(Test-Path $lic_path)
+    {
+    Remove-Item $lic_path -Recurse -Force -ErrorAction SilentlyContinue
+    }
+
 Remove-Item $icon_name -ErrorAction SilentlyContinue
--- drivesnapshot/tools/chocolateyinstall.ps1
+++ drivesnapshot/tools/chocolateyinstall.ps1
@@ -6,6 +6,9 @@ $url64       = 'http://www.drivesnapshot.de/download/snapshot64.exe'
 $checksum32  = '34db146f24f32e367835d6a9e8f22f9f8db3c6cd9e48fbda261dfc15178a0aeb'
 $checksum64  = '9db2688aee69dc69ec776b34daba226f4f2ca89682b771e2f41e0ddf21725638'
 $toolsPath   = (Split-Path $MyInvocation.MyCommand.Definition)
+$bits = Get-ProcessorBits
+$lic_path = "$env:USERPROFILE\drivesnapshot_lic"
+$lic=(Get-ChildItem $lic_path -Filter '*.txt' -ErrorAction SilentlyContinue).FullName
  
 $packageArgs = @{
   packageName    = $packageName
@@ -20,7 +23,24 @@ $packageArgs = @{
 
 Get-ChocolateyWebFile @packageArgs -GetOriginalFileName
  
- 
 $FileFullPath = get-childitem $toolsPath -recurse -include *.exe | select -First 1
       
 Install-ChocolateyShortcut -shortcutFilePath "$env:ALLUSERSPROFILE\Microsoft\Windows\Start Menu\Programs\Drive Snapshot.lnk" $FileFullPath
+
+
+if(!(Test-Path $lic_path -ErrorAction SilentlyContinue))
+    {
+    New-Item $lic_path -ItemType Directory | %{$_.Attributes = "hidden"}
+    }
+    elseif($lic -ne $null)
+    {
+        if($bits -eq 64)
+            {
+            Start-ChocolateyProcessAsAdmin -Statements "--register:$lic" -ExeToRun "$toolsPath\snapshot64.exe"
+            }
+        else
+            {
+            Start-ChocolateyProcessAsAdmin -Statements "--register:$lic" -ExeToRun "$toolsPath\snapshot.exe"
+            }
+  }
+    
