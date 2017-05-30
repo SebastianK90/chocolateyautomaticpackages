4007db5d83c1c6b251023def858b4b808ce56452
sippi90 <kowatsch.sebastian@outlook.de>
2017-05-30 18:57:03 +0200

    test

--- cemu/tools/chocolateyinstall.ps1
+++ cemu/tools/chocolateyinstall.ps1
@@ -1,14 +1,14 @@
 $ErrorActionPreference = 'Stop'
 
 $packageName = 'cemu'
-$url32       = 'http://cemu.info/releases/cemu_1.7.4.zip'
-$checksum32  = ''
+$url32       = 'http://cemu.info/releases/cemu_1.7.5.zip'
+$checksum32  = '97C5ADC68C588C1358EBC1E4F729FC4DB505BC6E7DBE150C7F9ACEA2ABE16757'
 $toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
 $cemu_folder = "$toolsPath\cemu"
 
 $packageArgs = @{
   packageName    = $packageName
-  url32            = $url32
+  url32           = $url32
   checksum32       = $checksum32
   checksumType   = 'sha256'
   unzipLocation  = $toolsPath
