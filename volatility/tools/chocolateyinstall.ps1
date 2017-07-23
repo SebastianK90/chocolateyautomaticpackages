b8e8a29127843a768e69e83624958254027d6f43
sippi90 <kowatsch.sebastian@outlook.de>
2017-07-23 21:19:27 +0200

    init

---
+++ volatility/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = 'volatility'
+$url32       = 'http://downloads.volatilityfoundation.org/releases/2.6/volatility_2.6_win64_standalone.zip'
+$checksum32  = 'bb021f3b569bf8ee4a408b2e07b0662699894ff7eecd4473badf0ef0c58f2fce'
+$toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  packageName    = $packageName
+  url            = $url32
+  checksum       = $checksum32
+  checksumType   = 'sha256'
+  unzipLocation  = $toolsPath
+}
+Install-ChocolateyZipPackage @packageArgs
---
+++ volatility/update.ps1
@@ -0,0 +1,25 @@
+import-module au
+
+
+$releases = 'https://github.com/volatilityfoundation/volatility/releases'
+
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+     }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases
+    $re      = '*.zip'
+    $url     = $download_page.links | ? href -like $re | select -First 1 -expand href
+    $version = [regex]::match($url,'[0-9]+(\.[0-9]+)*').value
+    $url32   = 'http://downloads.volatilityfoundation.org/releases/' + $version + '/volatility_' + $version + '_win64_standalone.zip'
+    return @{ URL32 = $url32; Version = $version }
+}
+
+update
