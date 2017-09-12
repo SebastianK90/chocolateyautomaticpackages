a0632cc664a2ce4a6c48fc3a83f1c4c56b3d5ad4
Chocolatey <chocolatey@realdimensions.net>
2017-09-12 12:09:44 +0000

    AU: 1 updated - ccleaner.portable [skip ci] https://gist.github.com/a80d008be848e4754c459284f24be9fa/21f9e8c164708ae3032ee354f32c0255916b58cf

--- ccleaner.portable/tools/chocolateyinstall.ps1
+++ ccleaner.portable/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName = 'ccleaner.portable'
 $url32       = 'https://www.piriform.com/ccleaner/download/portable/downloadfile'
-$checksum32  = 'e710744b3cdd16bda28327500e1f2adc395b7ecffe78d5ffb5c9371906594efa'
+$checksum32  = '81876d8419b8ec2c269b8917d56889d6625a78b817ea71a127664d3abea2d1d2'
 $toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
 $bits = Get-ProcessorBits
 $ccleaner_temp = "$env:USERPROFILE\ccleaner_temp"
