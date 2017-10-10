1e5628754f885c6890b923e846dff408b28aeafa
sippi90 <sippi90@users.noreply.github.com>
2017-10-10 09:22:26 +0200

    update checksums

--- ecm/tools/chocolateyinstall.ps1
+++ ecm/tools/chocolateyinstall.ps1
@@ -1,17 +1,21 @@
-﻿$ErrorActionPreference = 'Stop'
+$ErrorActionPreference = 'Stop'
  
 $packageName = 'ecm'
-$url32       = 'http://www.sordum.org/files/downloads.php?easy-context-menu'
-$checksum32  = '8B69073969F4E499A009C4CE90CB5E6CD990E2C07F124A646D6A0B693F855990'
+$url32       = 'https://www.sordum.org/files/easy-context-menu/ec_menu.zip'
+$checksum32  = '567DADB389FEC59529E2ADA6B9687C9897C44DC4B663D086CDAD044D0067724C'
 $toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
 $options =
 @{
   Headers = @{
-    'Accept' = 'text/html, application/xhtml+xml, image/jxr, */*';
-    'Accept-Language' = 'de-DE,de;q=0.5';
-    'Referer' = 'http://www.sordum.org/7615/';
-    'User-Agent' = 'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko';
+    'GET' = 'http://www.sordum.org/files/easy-context-menu/ec_menu.zip HTTP/1.1'
+    'Accept' = 'text/html, application/xhtml+xml, image/jxr, */*'
+    'Referer'= 'http://www.sordum.org/downloads/?easy-context-menu'
+    'Accept-Language'= 'de-DE,de;q=0.5'
+    'User-Agent' = 'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko'
     'Accept-Encoding' = 'gzip, deflate'
+    'Connection' = 'Keep-Alive'
+    'Pragma' = 'no-cache'
+    'DNT' = '1'
   }
 }
  
