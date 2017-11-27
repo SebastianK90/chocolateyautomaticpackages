8bac2f6d6c742ee1d036834006b8175447fc2615
sippi90 <sippi90@users.noreply.github.com>
2017-11-27 12:10:38 +0100

    avoid vector smash

--- mp3gain-gui/update.ps1
+++ mp3gain-gui/update.ps1
@@ -1,7 +1,5 @@
 import-module au
 
-sp 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3' 1A10 0
-
 $releases = 'https://sourceforge.net/projects/mp3gain/files/MP3Gain-Windows%20%28Stable%29/'
 
 function global:au_SearchReplace {
@@ -16,11 +14,9 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-    
-    $version = $download_page.links | Where-Object {$_.outerHTML -like '*/projects/mp3gain/files/MP3Gain*'} | Select-Object -ExpandProperty outerText -First 1
-    $url = $download_page.links | Where-Object {$_.outerHTML -like '*/projects/mp3gain/files/MP3Gain*'} | Select-Object -ExpandProperty href -First 1
+    $version = ($download_page.links | Where-Object {$_ -like '*Click to enter*'} | Select-Object -ExpandProperty title -First 1).substring(15)
     $url32   = 'https://sourceforge.net'+$url +'mp3gain-win-full-'+$version.Replace(".",'_')+ '.exe/download'
 
     return @{ URL32 = $url32; Version = $version }
