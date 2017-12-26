4c445311bb0394194c7957495a6c9ab396b027f1
sippi90 <kowatsch.sebastian@outlook.de>
2017-12-26 21:32:37 +0100

    init occt

---
+++ occt.portable/tools/chocolateyuninstall.ps1
@@ -0,0 +1,3 @@
+﻿Get-Process Occt* | Stop-Process -Force -ErrorAction SilentlyContinue
+$icon_name = (Get-ChildItem "$env:ALLUSERSPROFILE\Microsoft\Windows\Start Menu\Programs\" -Filter "OCCT.lnk" -ErrorAction SilentlyContinue).FullName
+Remove-Item $icon_name -ErrorAction SilentlyContinue
