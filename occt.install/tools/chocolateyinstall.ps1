1d195fd231b36150675c49fcf0ec9e8024139c04
sippi90 <kowatsch.sebastian@outlook.de>
2017-12-26 23:07:02 +0100

    fix

--- occt.install/tools/chocolateyinstall.ps1
+++ occt.install/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$packageName = 'occt.portable'
+$packageName = 'occt.install'
 $url32       = 'http://www.ocbase.com/download/OCCTPT4.5.1.exe'
 $checksum32  = '936457C564731C0FAB37E869C884F2D13F2115966C3FBD24C9167380BAF771BB'
 $toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
