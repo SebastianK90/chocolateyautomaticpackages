a154d4aca8b0168528e1a95034ea5b7f04c8da26
sippi90 <sippi90@users.noreply.github.com>
2018-04-24 15:45:24 +0200

    .

--- io-unlocker/update.ps1
+++ io-unlocker/update.ps1
@@ -15,7 +15,7 @@ function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases
     $content = $download_page.Content
 
-    $pattern = '(?<=class="button\ btn-jadegreen\ large">Free\ Download)[\S\s]*<dd><span>V (?<Version>[\d\.]+)'
+    $pattern = '(?<=class="button\ btn-jadegreen\ large")[\S\s]*<dd><span>V (?<Version>[\d\.]+)'
     $url32   = 'http://update.iobit.com/dl/unlocker-setup.exe'
     $version = [regex]::Match($content, $pattern).groups['Version'].value
     return @{ URL32 = $url32; Version = $version }
