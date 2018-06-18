973f55c5672546a82d8b45f7ffe9082d3e6ed43a
sippi90 <sippi90@users.noreply.github.com>
2018-06-18 09:48:18 +0200

    -UseBasicParsing

--- volatility/update.ps1
+++ volatility/update.ps1
@@ -1,8 +1,6 @@
 import-module au
 
-
 $releases = 'https://github.com/volatilityfoundation/volatility/releases'
-
 
 function global:au_SearchReplace {
     @{
@@ -14,7 +12,7 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
     $re      = '*.zip'
     $url     = $download_page.links | ? href -like $re | select -First 1 -expand href
     $version = [regex]::match($url,'[0-9]+(\.[0-9]+)*').value
