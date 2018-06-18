7948931dbde64a5b606ea2187a3f273fba7648a1
sippi90 <sippi90@users.noreply.github.com>
2018-06-18 09:47:54 +0200

    -UseBasicParsing

--- rambox/update.ps1
+++ rambox/update.ps1
@@ -14,7 +14,7 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
     $re64      = '*x64-win.zip'
     $re32      = '*ia32-win.zip'
