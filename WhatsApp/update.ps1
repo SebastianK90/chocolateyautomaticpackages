3bf87a9d0216045a6e04d7f23630ec3c60ff6ca3
sippi90 <sippi90@users.noreply.github.com>
2018-06-18 09:28:33 +0200

    update

--- WhatsApp/update.ps1
+++ WhatsApp/update.ps1
@@ -1,8 +1,6 @@
-﻿import-module au
+import-module au
 
-# cd .\whatsapp
-
-#$releases = 'http://www.softpedia.com/get/Internet/Chat/Instant-Messaging/WhatsApp.shtml'
+    
 function global:au_SearchReplace {
     @{
         'tools\chocolateyInstall.ps1' = @{
@@ -14,16 +12,12 @@ function global:au_SearchReplace {
      }
 }
 
-function global:au_GetLatest {
-    #$download_page = Invoke-WebRequest -Uri $releases
-    $useragents = @('Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko',`
-    'Mozilla/5.0 (X11; Linux x86_64; rv:17.0) Gecko/20121202 Firefox/17.0 Iceweasel/17.0.1',`
-    'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1',`
-    'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',`
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',`
-    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2750.0 Iron Safari/537.36'
-)
-    $random = Get-Random -Maximum 6
+ function global:au_GetLatest {   
+
+    $useragents = @('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246',`
+    'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3112 Safari/537.36',`
+    'Mozilla/5.0 (Windows NT x.y; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0')
+    $random = Get-Random -Maximum 3
     
     $url64   = 'https://web.whatsapp.com/desktop/windows/release/x64/WhatsAppSetup.exe'
     $url32   = 'https://web.whatsapp.com/desktop/windows/release/ia32/WhatsAppSetup.exe'
@@ -31,8 +25,6 @@ function global:au_GetLatest {
     Invoke-WebRequest -Uri $url32 -OutFile $output -UserAgent [$useragents[$random]]
     $version = ((Get-ChildItem $output).VersionInfo).fileversion
     Remove-Item $output
-    #$version = [regex]::match(($download_page.Links.innerhtml | Where-Object {$_ -like '*<span>Whatsapp*'}),'[0-9]+(\.[0-9]+)*').value
-    #$version = '0.2.2245'
     
     return @{ URL64 = $url64; URL32 = $url32; Version = $version }
 }
