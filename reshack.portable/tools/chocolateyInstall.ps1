ad209e6b0dabe811e3b95b22bb45ed6cf9784b0c
Chocolatey <chocolatey@realdimensions.net>
2018-07-15 00:08:31 +0000

    AU: 1 updated - reshack.portable [skip ci] https://gist.github.com/a80d008be848e4754c459284f24be9fa/bc8bbbae0aa82770a87f71d7e1e193e32de7b00f

--- reshack.portable/tools/chocolateyInstall.ps1
+++ reshack.portable/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName = 'reshack.portable'
 $url32       = 'http://www.angusj.com/resourcehacker/resource_hacker.zip'
-$checksum32  = '0c2ad1d93d3ed12c3d092f36ab5b0ccf3b70d4d3d50856eba76a6de36e16e921'
+$checksum32  = '50dabaf092ee11561ef49fb0648334748932297a63faf0b42d2a52c613ce645e'
 $toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
