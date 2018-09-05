4ef3690e91f14f3225efbbe68c2cdd65a833cef3
sippi90 <sippi90@users.noreply.github.com>
2018-09-05 16:03:14 +0200

    fixed download link

--- cloneapp/update.ps1
+++ cloneapp/update.ps1
@@ -16,7 +16,7 @@ function global:au_GetLatest {
     $content = $download_page.Content
 
     $pattern = '(?<=</div>\ <!--\ end\ of\ screenshot\ zone\ -->)[\S\s]*Version: (?<Version>[\d\.]+)'
-    $url32 = 'http://www.mirinsoft.com/index.php/downloads/cloneapp/send/2-cloneapp/19-cloneapp'
+    $url32 = 'http://www.mirinsoft.com/index.php/download/send/2-cloneapp/19-cloneapp'
     $version = [regex]::Match($content, $pattern).groups['Version'].value
     return @{ URL32 = $url32; Version = $version }
 }
