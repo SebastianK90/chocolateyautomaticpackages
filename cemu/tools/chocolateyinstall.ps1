037915ef79b912a2b9271d61cc5fae285ed3ea41
Chocolatey <chocolatey@realdimensions.net>
2018-10-03 18:12:16 +0000

    AU: 1 updated - cemu [skip ci] https://gist.github.com/a80d008be848e4754c459284f24be9fa/f3112960bcf81ddf9e53c8b5fdd8d3b8cf59d5a7

--- cemu/tools/chocolateyinstall.ps1
+++ cemu/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'cemu'
-$url32       = 'http://cemu.info/releases/cemu_1.13.1.zip'
-$checksum32  = '5fb86da5d5840d4a03bc75f4c86646f837312d4105ae499bd16aa4915cb77b7d'
+$url32       = 'http://cemu.info/releases/cemu_1.13.2.zip'
+$checksum32  = '24a22a1070d482dc39ae5a983022b5b824f3aa91b9d3d850d49a82f049559061'
 $toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
 $cemu_folder = "$toolsPath\cemu"
 
