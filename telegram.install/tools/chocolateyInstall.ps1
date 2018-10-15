61ec96372e3a2103ec9818d45f85b690fa0baec5
sippi90 <sippi90@users.noreply.github.com>
2018-10-15 22:19:48 +0200

    +32

--- telegram.install/tools/chocolateyInstall.ps1
+++ telegram.install/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = 'telegram.install'
 $installerType = 'exe'
-$url = 'https://github.com/telegramdesktop/tdesktop/releases/download/v1.3.10/tsetup.1.3.10.exe'
+$url32 = 'https://github.com/telegramdesktop/tdesktop/releases/download/v1.3.10/tsetup.1.3.10.exe'
 $checksum = '0ee3249d6b251f848a3b9d7a28c960192c64b31beace9428852c984387832c14'
 $checksumType = 'sha256'
 $silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
