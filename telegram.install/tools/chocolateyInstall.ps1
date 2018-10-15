d798ca7b259bc1403677f92c6435a66de0adaa1a
sippi90 <sippi90@users.noreply.github.com>
2018-10-15 22:31:20 +0200

    .

--- telegram.install/tools/chocolateyInstall.ps1
+++ telegram.install/tools/chocolateyInstall.ps1
@@ -1,15 +1,18 @@
+$ErrorActionPreference = 'Stop'
+
 $packageName = 'telegram.install'
-$installerType = 'exe'
 $url32 = 'https://github.com/telegramdesktop/tdesktop/releases/download/v1.3.10/tsetup.1.3.10.exe'
 $checksum32 = '0ee3249d6b251f848a3b9d7a28c960192c64b31beace9428852c984387832c14'
-$checksumType = 'sha256'
-$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-$validExitCodes = @(0) 
-
-Install-ChocolateyPackage -PackageName "$packageName" `
-                          -FileType "$installerType" `
-                          -SilentArgs "$silentArgs" `
-                          -Url "$url" `
-                          -ValidExitCodes $validExitCodes `
-                          -Checksum "$checksum" `
-                          -ChecksumType "$checksumType"
+		  
+						  
+$packageArgs = @{
+  packageName            = $packageName
+  fileType               = 'exe'
+  url                    = $url32
+  silentArgs             = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  checksum               = $checksum32
+  checksumType           = 'sha256'
+  validExitCodes         = @(0)
+  registryUninstallerKey = $packageName
+}
+Install-ChocolateyPackage @packageArgs
--- telegram.portable/tools/chocolateyInstall.ps1
+++ telegram.portable/tools/chocolateyInstall.ps1
@@ -1,11 +1,19 @@
+$ErrorActionPreference = 'Stop'
+
 $packageName = 'telegram.portable'
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url32 = 'https://github.com/telegramdesktop/tdesktop/releases/download/v1.3.14/tportable.1.3.14.zip'
 $checksum32 = 'f26ff2acf8293e6bb47b5bf3201b22e1e4f34c6361af4ba3f010cb3fdd27fe6e'
-$checksumType = 'sha256'
+$toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
 
-Install-ChocolateyZipPackage -PackageName "$packageName" `
-                             -Url "$url" `
-                             -UnzipLocation "$toolsDir" `
-                             -Checksum "$checksum" `
-                             -ChecksumType "$checksumType"
+$packageArgs = @{
+  packageName    = $packageName
+  url            = $url32
+  checksum       = $checksum32
+  checksumType   = 'sha256'
+  unzipLocation  = $toolsPath
+}
+Install-ChocolateyZipPackage @packageArgs
+
+$FileFullPath = get-childitem $toolsPath -recurse -include *.exe | select -First 1
+     
+Install-ChocolateyShortcut -shortcutFilePath "$env:ALLUSERSPROFILE\Microsoft\Windows\Start Menu\Programs\Telegram.lnk" $FileFullPath
