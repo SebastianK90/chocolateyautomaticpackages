b84b01cb2870f5d539d79ff43972604e9cdf7b4f
sippi90 <sippi90@users.noreply.github.com>
2018-10-15 22:23:13 +0200

    +32 again

--- telegram.install/tools/chocolateyInstall.ps1
+++ telegram.install/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $packageName = 'telegram.install'
 $installerType = 'exe'
 $url32 = 'https://github.com/telegramdesktop/tdesktop/releases/download/v1.3.10/tsetup.1.3.10.exe'
-$checksum = '0ee3249d6b251f848a3b9d7a28c960192c64b31beace9428852c984387832c14'
+$checksum32 = '0ee3249d6b251f848a3b9d7a28c960192c64b31beace9428852c984387832c14'
 $checksumType = 'sha256'
 $silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 $validExitCodes = @(0) 
--- telegram.portable/tools/chocolateyInstall.ps1
+++ telegram.portable/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $packageName = 'telegram.portable'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url32 = 'https://github.com/telegramdesktop/tdesktop/releases/download/v1.3.14/tportable.1.3.14.zip'
-$checksum = 'f26ff2acf8293e6bb47b5bf3201b22e1e4f34c6361af4ba3f010cb3fdd27fe6e'
+$checksum32 = 'f26ff2acf8293e6bb47b5bf3201b22e1e4f34c6361af4ba3f010cb3fdd27fe6e'
 $checksumType = 'sha256'
 
 Install-ChocolateyZipPackage -PackageName "$packageName" `
