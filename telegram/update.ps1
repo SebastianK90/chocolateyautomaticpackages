e5b395655373d0e7acfab61986882d445b3ef5d6
sippi90 <sippi90@users.noreply.github.com>
2018-10-18 08:07:23 +0200

    ?

--- telegram/update.ps1
+++ telegram/update.ps1
@@ -1,11 +1,15 @@
+import-module au
+
 . $PSScriptRoot\..\telegram.install\update.ps1
 
 function global:au_SearchReplace {
-   @{
+  @{
         "$($Latest.PackageName).nuspec" = @{
             "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"$($Latest.Version)`""
         }
     }
-}
+ }
+
+ function global:au_BeforeUpdate { }
 
 update -ChecksumFor none
