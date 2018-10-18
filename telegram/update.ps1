0f2af17ea749b43316bc389e4799efe0e47e37af
sippi90 <sippi90@users.noreply.github.com>
2018-10-18 08:11:17 +0200

    .

--- telegram/update.ps1
+++ telegram/update.ps1
@@ -1,15 +1,24 @@
 import-module au
 
-. $PSScriptRoot\..\telegram.install\update.ps1
+$releases = 'https://github.com/telegramdesktop/tdesktop/releases/'
 
 function global:au_SearchReplace {
-  @{
+   @{
         "$($Latest.PackageName).nuspec" = @{
             "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"$($Latest.Version)`""
         }
     }
- }
+}
 
- function global:au_BeforeUpdate { }
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    $re      = 'tsetup.*.exe'
+    $url     = $download_page.links | ? href -match $re | select -First 1 -expand href
+    $url32   = 'https://github.com' + $url
+    $version = ($url -split '\/' | select -Index 5).Substring(1)
+
+    return @{Version = $version }
+}
 
 update -ChecksumFor none
