467f64ea0c34c3e1719a0bfeb3356899fd47e3e6
sippi90 <kowatsch.sebastian@outlook.de>
2018-10-22 12:40:34 +0200

    init

---
+++ setuserfta/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = 'setuserfta'
+$url32       = 'https://kolbi.cz/SetUserFTA.zip'
+$checksum32  = 'D551295C779BDB3750DDBA8E781C21A3DD42A55578F818E9C789B2BA1B4DCF47'
+$toolsPath   = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+
+$packageArgs = @{
+  packageName    = $packageName
+  url            = $url32
+  checksum       = $checksum32
+  checksumType   = 'sha256'
+  unzipLocation  = $toolsPath
+}
+Install-ChocolateyZipPackage @packageArgs
+  
+Install-ChocolateyPath $toolsPath
