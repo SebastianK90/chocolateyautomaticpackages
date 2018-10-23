815e99a0b25ebeaa3115ee6a8cbea669963f509f
sippi90 <sippi90@users.noreply.github.com>
2018-10-23 21:22:09 +0200

    .

--- unifiedremote/update.ps1
+++ unifiedremote/update.ps1
@@ -12,15 +12,9 @@ function global:au_SearchReplace {
 
  function global:au_GetLatest {   
 
-    $useragents = @('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246',`
-    'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3112 Safari/537.36',`
-    'Mozilla/5.0 (Windows NT x.y; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0')
-    $random = Get-Random -Maximum 3
-    
-    $url32   = 'https://www.unifiedremote.com/download/windows-setup'
+    $url32   = 'https://www.unifiedremote.com/download/windows'
     $output = "$env:TEMP\unifiedremote.exe"
-    Start-BitsTransfer -Source $url32 -Destination $output
-    #Invoke-WebRequest -Uri $url32 -OutFile $output -UserAgent [$useragents[$random]] -UseBasicParsing
+    Start-BitsTransfer -Source $url32 -Destination $output -TransferType Download
     Set-Location $env:TEMP
     $version = ((Get-ChildItem .\unifiedremote.exe).VersionInfo).FileVersion
     Remove-Item $output
@@ -28,7 +22,4 @@ function global:au_SearchReplace {
     return @{URL32 = $url32; Version = $version }
 }
 
-update
-
-
-https://www.unifiedremote.com/download/windows-setup
+update -NoCheckUrl all
