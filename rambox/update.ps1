6fcab585ab7080b3f197caa1561d2a50916560bd
sippi90 <sippi90@users.noreply.github.com>
2018-10-24 14:21:11 +0200

    latest stable
    switched to latest stable instead of pre-versions

--- rambox/update.ps1
+++ rambox/update.ps1
@@ -1,6 +1,6 @@
 import-module au
 
-$releases = 'https://github.com/ramboxapp/community-edition/releases'
+$releases = 'https://github.com/ramboxapp/community-edition/releases/latest'
 
 function global:au_SearchReplace {
     @{
