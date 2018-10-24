4c4d8053fc73ad515cacb214637efb9f07aa1961
sippi90 <sippi90@users.noreply.github.com>
2018-10-24 14:48:22 +0200

    working dir

--- ChanSort/tools/chocolateyinstall.ps1
+++ ChanSort/tools/chocolateyinstall.ps1
@@ -16,4 +16,4 @@ Install-ChocolateyZipPackage @packageArgs
 
 $FileFullPath = get-childitem $toolsPath -recurse -include *.exe | select -First 1
   
-Install-ChocolateyShortcut -shortcutFilePath "$env:ALLUSERSPROFILE\Microsoft\Windows\Start Menu\Programs\ChanSort.lnk" $FileFullPath
+Install-ChocolateyShortcut -shortcutFilePath "$env:ALLUSERSPROFILE\Microsoft\Windows\Start Menu\Programs\ChanSort.lnk" $FileFullPath -WorkingDirectory "$toolsPath"
