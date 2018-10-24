b95529d0054e952ca30989f007c62f99ccb0b02c
sippi90 <kowatsch.sebastian@outlook.de>
2018-10-24 08:36:41 +0200

    .

--- unifiedremote/tools/chocolateyInstall.ps1
+++ unifiedremote/tools/chocolateyInstall.ps1
@@ -4,7 +4,6 @@ $packageName = 'unifiedremote'
 $url32       = 'https://www.unifiedremote.com/download/windows-setup'
 $checksum32  = '477292CC343800BAC042E854EECF0A7EE65E3575CF581D40E83A272128031306'
 $toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
- 
 $packageArgs = @{
   packageName            = $packageName
   fileType               = 'exe'
@@ -16,5 +15,5 @@ $packageArgs = @{
   registryUninstallerKey = $packageName
 }
 
-Start-Process autohotkey.exe -ArgumentList "$toolsPath\install.ahk" -PassThru
-Install-ChocolateyPackage @packageArgs
+Get-ChocolateyWebFile @packageArgs -fileFullPath "$toolsPath\installer.exe"
+Start-Process autohotkey.exe -ArgumentList "$toolsPath\install.ahk"
--- unifiedremote/tools/install.ahk
+++ unifiedremote/tools/install.ahk
@@ -1,4 +1,8 @@
 #NoTrayIcon
+#NoEnv  ; Recommended for performance and compatibility with future AutoHotkey releases.
+SendMode Input  ; Recommended for new scripts due to its superior speed and reliability.
+SetWorkingDir %A_ScriptDir%  ; Ensures a consistent starting directory.
+Run installer.exe /Verysilent
 sleep 1000
 WinWait, Windows ahk_class #32770 ahk_exe rundll32.exe, , 40
 SetControlDelay -1
