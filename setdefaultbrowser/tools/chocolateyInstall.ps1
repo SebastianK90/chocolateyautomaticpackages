897035a09a7e1969de6fbf1be472c04e475cedbc
sippi90 <kowatsch.sebastian@outlook.de>
2018-10-25 07:56:37 +0200

    init

---
+++ setdefaultbrowser/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = 'setdefaultbrowser'
+$url32       = 'https://kolbi.cz/SetDefaultBrowser.zip'
+$checksum32  = '7aa596efef5635589da4ceba36dfa9555deab6df5093be109b1ca5eef6c63681'
+$toolsPath   = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+
+$packageArgs = @{
+  packageName    = $packageName
+  url            = $url32
+  checksum       = $checksum32
+  checksumType   = 'sha256'
+  unzipLocation  = $toolsPath
+}
+
+Install-ChocolateyZipPackage @packageArgs
---
+++ setdefaultbrowser/update.ps1
@@ -0,0 +1,29 @@
+﻿import-module au
+
+
+    
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+     }
+}
+
+
+function global:au_GetLatest {
+
+    $url32     = 'https://kolbi.cz/SetDefaultBrowser.zip'
+    $output = "$env:TEMP\SetDefaultBrowser.zip"
+    Invoke-WebRequest -Uri $url32 -OutFile $output
+    Set-Location $env:TEMP
+    Expand-Archive "$env:TEMP\SetDefaultBrowser.zip" -Force
+    $version = ((Get-ChildItem $env:TEMP\SetDefaultBrowser\SetDefaultBrowser\SetDefaultBrowser.exe).VersionInfo).FileVersion
+    Remove-Item $output
+    
+    return @{URL32 = $url32; Version = $version }
+}
+
+
+update
