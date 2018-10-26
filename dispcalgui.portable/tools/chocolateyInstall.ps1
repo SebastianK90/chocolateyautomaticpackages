6287923fbda8d622b8eb12420260ae370b82b69b
sippi90 <kowatsch.sebastian@outlook.de>
2018-10-26 14:23:11 +0200

    .

--- dispcalgui.portable/tools/chocolateyInstall.ps1
+++ dispcalgui.portable/tools/chocolateyInstall.ps1
@@ -14,6 +14,6 @@ $packageArgs = @{
 }
 Install-ChocolateyZipPackage @packageArgs
 
-$FileFullPath = get-childitem $toolsPath -recurse -include *.exe | select -First 1
+$FileFullPath = get-childitem $toolsPath -recurse -include DisplayCAL.exe | select -First 1
   
 Install-ChocolateyShortcut -shortcutFilePath "$env:ALLUSERSPROFILE\Microsoft\Windows\Start Menu\Programs\DisplayCAL.lnk" $FileFullPath -WorkingDirectory "$toolsPath"
