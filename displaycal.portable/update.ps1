e6eebcf66412452a0a11b3c29aa95c2cfb408115
sippi90 <kowatsch.sebastian@outlook.de>
2018-10-26 14:36:54 +0200

    init

---
+++ displaycal.portable/update.ps1
@@ -0,0 +1,19 @@
+﻿import-module au
+
+$releases = 'https://sourceforge.net/projects/dispcalgui/files/release/'
+
+function global:au_SearchReplace {
+  @{
+        "$($Latest.PackageName).nuspec" = @{
+            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"[$($Latest.Version)]`""
+        }
+    }
+ }
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $version = ($download_page.links | Where-Object {$_ -like '*Click to enter*'} | Select-Object -ExpandProperty title -First 1).substring(15)
+    return @{Version = $version }
+}
+
+update -ChecksumFor none
