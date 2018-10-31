d47c0e51e4436329c779c0af027a118eee0af62b
sippi90 <sippi90@users.noreply.github.com>
2018-10-31 17:46:44 +0100

    init

---
+++ syspin/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'
+ 
+$packageName = 'syspin'
+$url32       = 'http://www.technosys.net/download.aspx?file=syspin.exe'
+$checksum32  = 'A2A4ECD19AF6717BC21F806EB74F2589F989B509D1625AF6D802B71DFF677D88'
+$toolsPath   = (Split-Path $MyInvocation.MyCommand.Definition)
+ 
+$packageArgs = @{
+  packageName    = $packageName
+  url            = $url32
+  checksum       = $checksum32
+  checksumType   = 'sha256'
+  FileFullPath  = "$toolsPath\syspin.exe"
+}
+
+Get-ChocolateyWebFile @packageArgs -GetOriginalFileName
---
+++ syspin/update.ps1
@@ -0,0 +1,24 @@
+import-module au
+
+    
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+     }
+}
+
+ function global:au_GetLatest {   
+
+    $url32   = 'http://www.technosys.net/download.aspx?file=syspin.exe'
+    $output = "$env:TEMP\syspin.exe"
+    Invoke-WebRequest -Uri $url32 -OutFile $output
+    $version = ((Get-Item $output).VersionInfo).FileVersion
+    Remove-Item $output
+    
+    return @{URL32 = $url32; Version = $version }
+}
+
+update
