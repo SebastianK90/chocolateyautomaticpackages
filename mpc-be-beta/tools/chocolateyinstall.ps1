21a944ed6512db9fe3f1ae986edb16c473adaeb5
sippi90 <sippi90@users.noreply.github.com>
2018-11-01 14:46:46 +0100

    .

--- mpc-be-beta/tools/chocolateyinstall.ps1
+++ mpc-be-beta/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $packageName = 'mpc-be'
 $fileType    = 'EXE'
 $url32       = 'https://sourceforge.net/projects/mpcbe/files/MPC-BE/Nightly%20Builds%20%28from%20svn%20trunk%29/1.5.2%20%28build%203973%29%20beta/MPC-BE.1.5.2.3973.x86-installer.zip/download'
 $url64       = 'https://sourceforge.net/projects/mpcbe/files/MPC-BE/Nightly%20Builds%20%28from%20svn%20trunk%29/1.5.2%20%28build%203973%29%20beta/MPC-BE.1.5.2.3973.x64-installer.zip/download'
-$checksum32  = '2D806AF7DFBB922308232896CE1CB370E40D0C17A7B8734C2040482AAAD50AA1'
-$checksum64  = '4CE2B4C9C6F43210B473CEA522BCE38C9968682A7F8203C47A90C9D48CA39B27'
+$checksum32  = '2d806af7dfbb922308232896ce1cb370e40d0c17a7b8734c2040482aaad50aa1'
+$checksum64  = '4ce2b4c9c6f43210b473cea522bce38c9968682a7f8203c47a90c9d48ca39b27'
 $toolsPath   = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 $silentArgs  = '/VERYSILENT /NORESTART /SUPPRESSMSGBOXES /SP-'
 
