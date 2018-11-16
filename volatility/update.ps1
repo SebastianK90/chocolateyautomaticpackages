b968b7b605f4252f9084dbb75ead9dd61cb90bc4
sippi90 <sippi90@users.noreply.github.com>
2018-11-16 10:53:48 +0100

    fixed url32

--- volatility/update.ps1
+++ volatility/update.ps1
@@ -15,7 +15,7 @@ function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
     $re      = '*_standalone.zip'
     $url32     = $download_page.links | ? href -like $re | select -First 1 -expand href
-    $version = [regex]::match($url,'[0-9]+(\.[0-9]+)*').value
+    $version = [regex]::match($url32,'[0-9]+(\.[0-9]+)*').value
     return @{ URL32 = $url32; Version = $version }
 }
 
