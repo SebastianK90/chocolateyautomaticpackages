5e49c0d4774cdb2975ae4cc2d8293e3190c8fb6a
sippi90 <kowatsch.sebastian@outlook.de>
2018-12-03 09:54:21 +0100

    fixed version detection...again

--- dispcalgui.install/update.ps1
+++ dispcalgui.install/update.ps1
@@ -13,20 +13,22 @@ function global:au_SearchReplace {
 
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-    $latest2 = ($download_page.links | Where-Object {$_ -like '*Click to enter*'} | Select-Object -ExpandProperty title -First 3).substring(15)
-    if ($url32 = ((Invoke-WebRequest -Uri ($releases + $latest2[0]) | Where-Object {$_.href -like "*Setup.exe/download*"}).href))
+    $latest = ($download_page.links | Where-Object {$_ -like '*Click to enter*'} | Select-Object -ExpandProperty title -First 3).substring(15)
+    $url32 = (Invoke-WebRequest -Uri ($releases + $latest[0])).Links
+    
+   if ($url32 = ((Invoke-WebRequest -Uri ($releases + $latest[0])).Links | Where-Object ({$_.href -like "*Setup.exe/download*"})).href)
     {
 
         [string]$version =  [regex]::match($url32,'[0-9]+(\.[0-9]+)*').value
 
     }
-elseif($url32 = ((Invoke-WebRequest -Uri ($releases + $latest2[1]) -UseBasicParsing).Links | Where-Object {$_.href -like "*Setup.exe/download*"}).href)
+  elseif($url32 = ((Invoke-WebRequest -Uri ($releases + $latest[1])).Links | Where-Object ({$_.href -like "*Setup.exe/download*"})).href)
 
     {
        [string]$version =  [regex]::match($url32,'[0-9]+(\.[0-9]+)*').value
     }
 
-elseif($url32 = ((Invoke-WebRequest -Uri ($releases + $latest2[2]) -UseBasicParsing).Links | Where-Object {$_.href -like "*Setup.exe/download*"}).href)
+  elseif($url32 = ((Invoke-WebRequest -Uri ($releases + $latest[2])).Links | Where-Object ({$_.href -like "*Setup.exe/download*"})).href)
 
     {
        [string]$version =  [regex]::match($url32,'[0-9]+(\.[0-9]+)*').value
