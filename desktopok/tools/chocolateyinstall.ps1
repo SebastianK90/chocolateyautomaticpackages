473e20a99a7cd3091bc8a290f32b391d9371e54f
Chocolatey <chocolatey@realdimensions.net>
2019-01-19 12:07:15 +0000

    AU: 1 updated - desktopok [skip ci] https://gist.github.com/a80d008be848e4754c459284f24be9fa/efb83484e4573f4d030769a9720bd6e27b233fb3

--- desktopok/tools/chocolateyinstall.ps1
+++ desktopok/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageName = 'desktopok'
 $url32       = 'http://www.softwareok.com/Download/DesktopOK.zip'
 $url64       = 'http://www.softwareok.com/Download/DesktopOK_x64.zip'
-$checksum32  = 'eba1e4adbc6f3f07dd71cabaddf05631f9eb628ae0ddc37922d49efee4cb8e5b'
-$checksum64  = '617673257f3e04ae66edbd1dfb086eca82777f9638deb6c54fd5b4ed240b8ca1'
+$checksum32  = 'eb63816f7e0ec9d592558a798f294905da55939cb0b4fcdc95d7fe4919c97256'
+$checksum64  = '7dce8dd459d4723df8b7ea5d5f18d28b9b92103cdfd1a064b847c4737f298b20'
 $toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
