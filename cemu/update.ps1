ce8286ef977c3e9d2e8537e0759104a9dfc466bc
sippi90 <sippi90@users.noreply.github.com>
2019-02-09 17:19:41 +0100

    those damn letters -.-

--- cemu/update.ps1
+++ cemu/update.ps1
@@ -12,23 +12,27 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases
+  $download_page = Invoke-WebRequest -Uri $releases
 
-    $re      = '*cemu*.zip'
-    $url64  = $download_page.links | ? href -like $re | select -First 1 -expand href
-    $url32 = $url64
-    $versionGroups = [regex]::Match($url32,'(?<Version>[0-9]+(?:\.[0-9]+)*)(?<Revision>[A-Za-z]*)').Groups
-    $version = $versionGroups['Version'].Value
+  $re      = '*cemu*.zip'
+  $url64  = $download_page.links | ? href -like $re | select -First 1 -expand href
+  $url32 = $url64
+  $pattern = '(?<=Latest\ release)[\S\s]*font-big custom">Cemu (?<Version>[\d\.]+\D)'
+  $version = [regex]::Match($download_page.content, $pattern).groups['Version'].value
+  
+  
+  $versionGroups = [regex]::Match($version,'(?<Version>[0-9]+(?:\.[0-9]+)*)(?<Revision>[A-Za-z]*)').Groups
+  $version = $versionGroups['Version'].Value
+        
+  $revision = 0
+  foreach ($c in $versionGroups['Revision'].Value.ToLowerInvariant().ToCharArray()) {
+    $revision *= 26
+    $revision += [int]$c - [int][char]'a'
+  }
+  $revision *= 100
+  $version += '.' + $revision
 
-    $revision = 0
-    foreach ($c in $versionGroups['Revision'].Value.ToLowerInvariant().ToCharArray()) {
-        $revision *= 26
-        $revision += [int]$c - [int][char]'a'
-    }
-    $revision *= 100
-    $version += '.' + $revision
-
-    return @{ URL32 = $url32; Version = $version }
+  return @{ URL32 = $url32; Version = $version }
 }
 
 update -ChecksumFor 32
