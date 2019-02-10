9a82e4312809bc159e3a2a5b945b611f68eedd30
Chocolatey <chocolatey@realdimensions.net>
2019-02-10 18:07:34 +0000

    AU: 1 updated - ChanSort [skip ci] https://gist.github.com/a80d008be848e4754c459284f24be9fa/e1eaa620573ca0d31e4b7058f72694a2b2be46b1

--- ChanSort/tools/chocolateyinstall.ps1
+++ ChanSort/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'ChanSort'
-$url32       = 'https://github.com/PredatH0r/ChanSort/releases/download/v2017-11-30/ChanSort_2017-11-30.zip'
-$checksum32  = 'af2a7dcc34ae14316848c4d606dbcd5827900ca71bedc5cfc4088628ff92a908'
+$url32       = 'https://github.com/PredatH0r/ChanSort/releases/download/v2019-02-10/ChanSort_2019-02-10.zip'
+$checksum32  = 'dee24d938d8263e282e0d815d80a0ca814761d0c0ae29247011d49cf75556f89'
 $toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
