ec5f174d83d624163fb7b7810078a858caedcf07
Chocolatey <chocolatey@realdimensions.net>
2019-02-22 00:07:50 +0000

    AU: 1 updated - WhatsApp [skip ci] https://gist.github.com/a80d008be848e4754c459284f24be9fa/0538deea6fc7665d30412b8a365ea1bdd11563d0

--- WhatsApp/tools/chocolateyinstall.ps1
+++ WhatsApp/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageName = 'whatsapp'
 $url32       = 'https://web.whatsapp.com/desktop/windows/release/ia32/WhatsAppSetup.exe'
 $url64       = 'https://web.whatsapp.com/desktop/windows/release/x64/WhatsAppSetup.exe'
-$checksum32  = '4ad0601869b3a35856183806fdbd9d307e1bfe92c06e4e49b1e925cd683c425d'
-$checksum64  = '17931a42e2dc1df97230cf2e3ea9371802f9047226da11daea470b548d7053f4'
+$checksum32  = 'fb59a3a2c0430a43a922b5650ea4d8e3d13c535032c9575d888cfa8d0123949f'
+$checksum64  = '1317d3337d14a676372df76dd66399219d3f3a4b81abc38f8a5f07fcd6f1aad1'
 
 $packageArgs = @{
   packageName            = $packageName
