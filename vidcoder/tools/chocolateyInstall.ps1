0a5ee762a8750009c7286501e6674f8582a53538
sippi90 <sippi90@users.noreply.github.com>
2019-03-20 17:42:45 +0100

    init

---
+++ vidcoder/tools/chocolateyInstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = 'vidcoder'
+$url32       = 'https://github.com/RandomEngy/VidCoder/releases/download/v3.16/VidCoder-3.16.exe'
+$checksum32  = 'b3ded05a1542c9aef4e74501b9e9c077cee9ae34c1c0513729988093ce3725f3'
+
+$packageArgs = @{
+  packageName            = $packageName
+  fileType               = 'exe'
+  url                    = $url32
+  silentArgs             = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART'
+  checksum               = $checksum32
+  checksumType           = 'sha256'
+  validExitCodes         = @(0)
+  registryUninstallerKey = $packageName
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ vidcoder/update.ps1
@@ -0,0 +1,26 @@
+import-module au
+
+
+
+$releases = 'https://github.com/RandomEngy/VidCoder/releases/latest'
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+     }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    
+    $re      = 'VidCoder-.*.exe'
+    $url     = $download_page.links | ? href -match $re | select -First 1 -expand href
+    $url32   = 'https://github.com' + $url
+    $version = [regex]::match(($url -split '\/')[-1],'[0-9]+(\.[0-9]+)*').value
+    return @{ URL32 = $url32; Version = $version }
+}
+
+update
