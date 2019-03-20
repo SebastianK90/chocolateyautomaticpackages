4fb236b32dbf942cd2bf1ff0d6ab791cbb0093f6
sippi90 <sippi90@users.noreply.github.com>
2019-03-20 17:54:09 +0100

    fixed update script
    to not detect portable

--- vidcoder/update.ps1
+++ vidcoder/update.ps1
@@ -15,9 +15,8 @@ function global:au_SearchReplace {
 
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-    
     $re      = 'VidCoder-.*.exe'
-    $url     = $download_page.links | ? href -match $re | select -First 1 -expand href
+    $url     = ($download_page.links | Where-Object {($_.href -match $re) -and ($_.href -notlike '*Portable*')}).href
     $url32   = 'https://github.com' + $url
     $version = [regex]::match(($url -split '\/')[-1],'[0-9]+(\.[0-9]+)*').value
     return @{ URL32 = $url32; Version = $version }
