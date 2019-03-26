81592e373b9a2f641d329a336ff9c916e288fdfc
Chocolatey <chocolatey@realdimensions.net>
2019-03-26 00:07:56 +0000

    AU: 1 updated - EMDB [skip ci] https://gist.github.com/a80d008be848e4754c459284f24be9fa/cf37ffb9256b71dd3047020244ad5607ec63be1d

--- EMDB/tools/chocolateyinstall.ps1
+++ EMDB/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 $packageName = 'emdb'
 $fileType    = 'EXE'
 $url32       = 'http://www.emdb.eu/bin/emdb.zip'
-$checksum32  = '68aa64e2da9237f9c52038743ab9188a51e59b0a22247d4a10b357962563c9ba'
+$checksum32  = 'c4feba94606fce679bb8f568f41885771883dc647deadf1ea7b0dd687ce4219f'
 $toolsPath   = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 $silentArgs  = '/VERYSILENT /COMPONENTS="main,mpciconlib,mpcresources"'
 
