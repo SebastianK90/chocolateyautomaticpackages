4f1da722446d2f361ee184951996fce5ce7a5ff2
Chocolatey <chocolatey@realdimensions.net>
2019-04-02 19:16:28 +0000

    AU: 1 updated - wxmp3gain [skip ci] https://gist.github.com/a80d008be848e4754c459284f24be9fa/d19478b5baf2431a7f85777a468b4e552fca82ab

--- wxmp3gain/tools/chocolateyInstall.ps1
+++ wxmp3gain/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'wxmp3gain'
-$url32       = 'https://sourceforge.net/projects/wxmp3gain/files/3.7/wxmp3gain-v3.7-win32-setup.exe/download'
-$checksum32  = '4ddb5df248e8f52104d4455fc3279ade376b7131e21be5ef10da99b0b4b86239'
+$url32       = 'https://sourceforge.net/projects/wxmp3gain/files/4.0/wxmp3gain-4.0-win32-setup.exe/download'
+$checksum32  = 'a5f1879d9e2eacf104e4a8bfbd0361226ed2b991090514a7d8d9d46041c8e13e'
 $toolsPath   = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 
 $packageArgs = @{
