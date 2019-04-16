df1dd4c4157abf23d3587a45e02c41d2048735cb
Chocolatey <chocolatey@realdimensions.net>
2019-04-16 06:06:35 +0000

    AU: 1 updated - rambox [skip ci] https://gist.github.com/a80d008be848e4754c459284f24be9fa/c34a12ac7605f9e8636a922030a2ff62562d7cc0

--- rambox/tools/chocolateyinstall.ps1
+++ rambox/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'rambox'
-$url32       = 'https://github.com/ramboxapp/community-edition/releases/download/0.6.4/Rambox-0.6.4-win-ia32.zip'
-$url64       = 'https://github.com/ramboxapp/community-edition/releases/download/0.6.4/Rambox-0.6.4-win-x64.zip'
-$checksum32  = '5465171aae0ae53f6f522c4caffd42b551d06544f74732baf9362a30507be472'
-$checksum64  = '4ca8345a6c9890a2b8b68f788ede083cfb61a171d7ca7ac6cb523929f25161eb'
+$url32       = 'https://github.com/ramboxapp/community-edition/releases/download/0.6.6/Rambox-0.6.6-win-ia32.zip'
+$url64       = 'https://github.com/ramboxapp/community-edition/releases/download/0.6.6/Rambox-0.6.6-win-x64.zip'
+$checksum32  = 'f07c6176aac75380dac84049fa81c856fc6df722fe6d90a2667677b988a3bce3'
+$checksum64  = 'cac764962d72b758502ba79b47f26fb83379d2c88745141905ae44b1bf786a5b'
 $toolsPath   = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 
 $packageArgs = @{
