4ecc1def0830e6cf6429d8c2f6a69dc61a91b08f
sippi90 <sippi90@users.noreply.github.com>
2019-04-24 23:37:10 +0200

    first try with embedded packages

--- desktopok/tools/chocolateyinstall.ps1
+++ desktopok/tools/chocolateyinstall.ps1
@@ -1,25 +1,18 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$packageName = 'desktopok'
-$url32       = 'https://www.softwareok.com/Download/DesktopOK.zip'
-$url64       = 'https://www.softwareok.com/Download/DesktopOK_x64.zip'
-$checksum32  = 'eb3aea04f0ed204472f1c93096e8d3dec8b1e5fa197085741993179044258ed8'
-$checksum64  = '18cbd54368d14fb0aa5433ca7fd56cac4b0913206fd2343ec92216407a642a06'
-$toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
-  packageName    = $packageName
-  url            = $url32
-  url64Bit       = $url64
-  checksum       = $checksum32
-  checksum64     = $checksum64
-  checksumType   = 'sha256'
-  checksumType64 = 'sha256'
-  unzipLocation  = $toolsPath
+  PackageName    = 'desktopok'
+  FileFullPath   = Get-Item $toolsPath\*_x32.zip
+  FileFullPath64 = Get-Item $toolsPath\*_x64.zip
+  Destination    = $toolsPath
 }
-Install-ChocolateyZipPackage @packageArgs
 
+Get-ChildItem $toolsPath\* | Where-Object { $_.PSISContainer } | Remove-Item -Recurse -Force #remove older package dirs
+Get-ChocolateyUnzip @packageArgs
+Remove-Item $toolsPath\*.zip -ea 0
 
-$FileFullPath = get-childitem $toolsPath -recurse -include *.exe | select -First 1
-     
-Install-ChocolateyShortcut -shortcutFilePath "$env:ALLUSERSPROFILE\Microsoft\Windows\Start Menu\Programs\DesktopOK.lnk" $FileFullPath -WorkingDirectory "$toolsPath"
+$Shortcut = get-childitem $toolsPath -Recurse -Include *.exe | Select-Object -First 1
+      
+Install-ChocolateyShortcut -shortcutFilePath "$env:ALLUSERSPROFILE\Microsoft\Windows\Start Menu\Programs\DesktopOK.lnk" $Shortcut -WorkingDirectory "$toolsPath"
--- desktopok/update.ps1
+++ desktopok/update.ps1
@@ -1,29 +1,38 @@
-import-module au
+﻿import-module au
 
+$au_Push = $true
+$au_Force = $true
 
 $releases = 'https://www.softwareok.de/?seite=Freeware/DesktopOK'
 
 function global:au_SearchReplace {
-    @{
-        'tools\chocolateyInstall.ps1' = @{
-            "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
-            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
-        }
-     }
+   @{
+      ".\legal\VERIFICATION.txt" = @{
+        "(?i)(\s+x32:).*"            = "`${1} $($Latest.URL32)"
+        "(?i)(\s+x64:).*"            = "`${1} $($Latest.URL64)"
+        "(?i)(checksum32:).*"        = "`${1} $($Latest.Checksum32)"
+        "(?i)(checksum64:).*"        = "`${1} $($Latest.Checksum64)"
+      }
+    }
 }
 
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge }
+
 function global:au_GetLatest {
+
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
     $content = $download_page.Content
     $url64   = 'https://www.softwareok.com/Download/DesktopOK_x64.zip'
-    $url32   = 'https://www.softwareok.com/Download/DesktopOK.zip'
+    $url   = 'https://www.softwareok.com/Download/DesktopOK.zip'
     $pattern = '(?<=<html>)[\S\s]* <title>DesktopOK (?<Version>[\d\.]+)'
     $version = [regex]::Match($content, $pattern).groups['Version'].value
-    
-    return @{ URL64 = $url64; URL32 = $url32; Version = $version }
+
+    @{
+      Version = $version
+      URL32 = $url
+      URL64 = $url64
+    }
 }
 
 
-update -NoCheckUrl
+update -ChecksumFor none
