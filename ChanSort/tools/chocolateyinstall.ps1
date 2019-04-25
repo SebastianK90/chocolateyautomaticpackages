1b28d140117888105280e68698132f4c3d0e2e7b
sippi90 <sippi90@users.noreply.github.com>
2019-04-25 21:47:28 +0200

    next one

--- ChanSort/tools/chocolateyinstall.ps1
+++ ChanSort/tools/chocolateyinstall.ps1
@@ -1,19 +1,17 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$packageName = 'ChanSort'
-$url32       = 'https://github.com/PredatH0r/ChanSort/releases/download/v2019-02-10/ChanSort_2019-02-10.zip'
-$checksum32  = 'dee24d938d8263e282e0d815d80a0ca814761d0c0ae29247011d49cf75556f89'
-$toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
-  packageName    = $packageName
-  url            = $url32
-  checksum       = $checksum32
-  checksumType   = 'sha256'
-  unzipLocation  = $toolsPath
+  PackageName    = 'ChanSort'
+  FileFullPath   = Get-Item $toolsPath\*_x32.zip
+  Destination    = $toolsPath
 }
-Install-ChocolateyZipPackage @packageArgs
 
-$FileFullPath = get-childitem $toolsPath -recurse -include *.exe | select -First 1
-  
-Install-ChocolateyShortcut -shortcutFilePath "$env:ALLUSERSPROFILE\Microsoft\Windows\Start Menu\Programs\ChanSort.lnk" $FileFullPath -WorkingDirectory "$toolsPath"
+Get-ChildItem $toolsPath\* | Where-Object { $_.PSISContainer } | Remove-Item -Recurse -Force #remove older package dirs
+Get-ChocolateyUnzip @packageArgs
+Remove-Item $toolsPath\*.zip -ea 0
+
+$Shortcut = get-childitem $toolsPath -Recurse -Include *.exe | Select-Object -First 1
+      
+Install-ChocolateyShortcut -shortcutFilePath "$env:ALLUSERSPROFILE\Microsoft\Windows\Start Menu\Programs\ChanSort.lnk" $Shortcut -WorkingDirectory "$toolsPath"
--- ChanSort/update.ps1
+++ ChanSort/update.ps1
@@ -1,24 +1,40 @@
 import-module au
 
+$au_Push = $false	
+$au_Force = $true
+
+
 $releases = 'https://github.com/PredatH0r/ChanSort/releases'
 
 function global:au_SearchReplace {
-    @{
-        'tools\chocolateyInstall.ps1' = @{
-            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*packageName\s*=\s*)('.*')"  = "`$1'$($Latest.PackageName)'"
         }
-     }
+
+
+        ".\legal\VERIFICATION.txt" = @{
+          "(?i)(\s+x32:).*"            = "`${1} $($Latest.URL32)"
+          "(?i)(checksum32:).*"        = "`${1} $($Latest.Checksum32)"
+        }
+    }
 }
+
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge }
+
 
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
     $re      = '*ChanSort*.zip'
     $url     = $download_page.links | ? href -like $re | select -First 1 -expand href
-    $url32   = 'https://github.com' + $url
     [string]$version = (($url -split '\/' | select -Index 5).Substring(1)).replace('-',".")
-    return @{ URL32 = $url32; Version = $version }
+
+     @{
+        URL32        = 'https://github.com' + $url
+        Version      = $version
+    }
 }
 
-update
+
+Update-Package -ChecksumFor none
