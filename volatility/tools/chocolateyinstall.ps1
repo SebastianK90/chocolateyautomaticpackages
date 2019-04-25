c52a8d9c65bd535d7a05da891dba3763c67a31bb
sippi90 <sippi90@users.noreply.github.com>
2019-04-25 21:54:28 +0200

    next one

--- volatility/tools/chocolateyinstall.ps1
+++ volatility/tools/chocolateyinstall.ps1
@@ -1,15 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$packageName = 'volatility'
-$url32       = 'http://downloads.volatilityfoundation.org/releases/2.6/volatility_2.6_win64_standalone.zip'
-$checksum32  = 'bb021f3b569bf8ee4a408b2e07b0662699894ff7eecd4473badf0ef0c58f2fce'
-$toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
-  packageName    = $packageName
-  url            = $url32
-  checksum       = $checksum32
-  checksumType   = 'sha256'
-  unzipLocation  = $toolsPath
+  PackageName    = 'volatility'
+  FileFullPath   = Get-Item $toolsPath\*_x32.zip
+  Destination    = $toolsPath
 }
-Install-ChocolateyZipPackage @packageArgs
+
+Get-ChildItem $toolsPath\* | Where-Object { $_.PSISContainer } | Remove-Item -Recurse -Force #remove older package dirs
+Get-ChocolateyUnzip @packageArgs
+Remove-Item $toolsPath\*.zip -ea 0
--- volatility/update.ps1
+++ volatility/update.ps1
@@ -1,22 +1,38 @@
 import-module au
 
+$au_Push = $false	
+$au_Force = $true
+
 $releases = 'https://www.volatilityfoundation.org/releases'
 
 function global:au_SearchReplace {
-    @{
-        'tools\chocolateyInstall.ps1' = @{
-            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*packageName\s*=\s*)('.*')"  = "`$1'$($Latest.PackageName)'"
         }
-     }
+
+
+        ".\legal\VERIFICATION.txt" = @{
+          "(?i)(\s+x32:).*"            = "`${1} $($Latest.URL32)"
+          "(?i)(checksum32:).*"        = "`${1} $($Latest.Checksum32)"
+        }
+    }
 }
+
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge }
+
 
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
     $re      = '*_standalone.zip'
-    $url32     = $download_page.links | ? href -like $re | select -First 1 -expand href
-    $version = [regex]::match($url32,'[0-9]+(\.[0-9]+)*').value
-    return @{ URL32 = $url32; Version = $version }
+    $url     = $download_page.links | ? href -like $re | select -First 1 -expand href
+    $version = [regex]::match($url,'[0-9]+(\.[0-9]+)*').value
+
+     @{
+        URL32        = $url
+        Version      = $version
+    }
 }
 
-update
+
+Update-Package -ChecksumFor none
