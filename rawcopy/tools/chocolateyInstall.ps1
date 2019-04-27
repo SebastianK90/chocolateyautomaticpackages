e2e0d9eda331d0ed4cb404f37112c57290fee17d
sippi90 <sippi90@users.noreply.github.com>
2019-04-27 16:38:27 +0200

    rawcopy as embedded

--- rawcopy/tools/chocolateyInstall.ps1
+++ rawcopy/tools/chocolateyInstall.ps1
@@ -1,20 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$packageName = 'rawcopy'
-$url32       = 'http://ltr-data.se/files/rawcopy.zip'
-$url64       = 'http://ltr-data.se/files/win64/rawcopy64.zip'
-$checksum32  = 'a66d00d96c08291923dee1af5b9a61892e60dd80115602f3f3eaedc092372cef'
-$checksum64  = 'af94badd91afe11f3f4ec7b9b17d2e0a1aa52e6baf28500cd2a791351e1742ff'
-$toolsPath   = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
-  packageName    = $packageName
-  url            = $url32
-  url64Bit       = $url64
-  checksum       = $checksum32
-  checksum64     = $checksum64
-  checksumType   = 'sha256'
-  checksumType64 = 'sha256'
-  unzipLocation  = $toolsPath
+  PackageName    = 'rawcopy'
+  FileFullPath   = Get-Item $toolsPath\*_x32.zip
+  FileFullPath64 = Get-Item $toolsPath\*_x64.zip
+  Destination    = $toolsPath
 }
-Install-ChocolateyZipPackage @packageArgs
+
+Get-ChildItem $toolsPath\* | Where-Object { $_.PSISContainer } | Remove-Item -Recurse -Force #remove older package dirs
+Get-ChocolateyUnzip @packageArgs
+Remove-Item $toolsPath\*.zip -ea 0
--- rawcopy/update.ps1
+++ rawcopy/update.ps1
@@ -1,18 +1,20 @@
 import-module au
 
-    
+
 function global:au_SearchReplace {
-    @{
-        'tools\chocolateyInstall.ps1' = @{
-            "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
-            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
-        }
-     }
+   @{
+      ".\legal\VERIFICATION.txt" = @{
+        "(?i)(\s+x32:).*"            = "`${1} $($Latest.URL32)"
+        "(?i)(\s+x64:).*"            = "`${1} $($Latest.URL64)"
+        "(?i)(checksum32:).*"        = "`${1} $($Latest.Checksum32)"
+        "(?i)(checksum64:).*"        = "`${1} $($Latest.Checksum64)"
+      }
+    }
 }
 
- function global:au_GetLatest {   
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge}
+
+function global:au_GetLatest {
 
     $useragents = @('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246',`
     'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3112 Safari/537.36',`
@@ -23,13 +25,17 @@ function global:au_SearchReplace {
     $url32   = 'http://ltr-data.se/files/rawcopy.zip'
     $output = "$env:TEMP\rawcopy.zip"
     Invoke-WebRequest -Uri $url32 -OutFile $output -UserAgent [$useragents[$random]]
-    Set-Location $env:TEMP
-    Expand-Archive .\rawcopy.zip -Force
-    $ver = (Get-ChildItem .\rawcopy\rawcopy.exe).VersionInfo
+    Expand-Archive -Path $env:TEMP\rawcopy.zip -DestinationPath $env:TEMP\rawcopy -Force
+    $ver = (Get-ChildItem $env:TEMP\rawcopy\rawcopy.exe).VersionInfo
     $version = $ver.ProductVersionRaw.Major.ToString() + "." + $ver.ProductVersionRaw.Minor.ToString() + "." + $ver.ProductVersionRaw.Build.ToString() + "." + $ver.ProductVersionRaw.Revision.ToString()
-    Remove-Item $output
+    Remove-Item $output,$env:TEMP\rawcopy -Recurse -ea 0
     
-    return @{ URL64 = $url64; URL32 = $url32; Version = $version }
+    @{
+      Version = $version
+      URL32 = $url32
+      URL64 = $url64
+    }
 }
 
-update
+
+update -ChecksumFor none
