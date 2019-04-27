98a0173501665625042945902f08d339ce7eb2f7
sippi90 <sippi90@users.noreply.github.com>
2019-04-27 14:10:36 +0200

    next embedded

--- cloneapp/tools/chocolateyinstall.ps1
+++ cloneapp/tools/chocolateyinstall.ps1
@@ -1,46 +1,43 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$packageName = 'cloneapp'
-$url32       = 'http://www.mirinsoft.com/index.php/download/send/2-cloneapp/19-cloneapp'
-$checksum32  = 'f99da8a4865e529114df25906bb7fd6f56bf2219e658698f4becebceda7ee8da'
-$toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
-  packageName    = $packageName
-  url            = $url32
-  checksum       = $checksum32
-  checksumType   = 'sha256'
-  unzipLocation  = $toolsPath
+  PackageName    = 'cloneapp'
+  FileFullPath   = Get-Item $toolsPath\*_x32.zip
+  Destination    = $toolsPath
 }
-Install-ChocolateyZipPackage @packageArgs
 
-$FileFullPath = get-childitem $toolsPath -Recurse -Filter "CloneApp*" -include *.exe | select -First 1
-  
-Install-ChocolateyShortcut -shortcutFilePath "$env:ALLUSERSPROFILE\Microsoft\Windows\Start Menu\Programs\CloneApp.lnk" $FileFullPath -WorkingDirectory "$toolsPath"
+Get-ChildItem $toolsPath\* | Where-Object { $_.PSISContainer } | Remove-Item -Recurse -Force #remove older package dirs
+Get-ChocolateyUnzip @packageArgs
+Remove-Item $toolsPath\*.zip -ea 0
 
-Install-ChocolateyPath $toolsPath
-  
+$Shortcut = Get-ChildItem $toolsPath -Recurse -Include *.exe | Select-Object -First 1
+      
+Install-ChocolateyShortcut -shortcutFilePath "$env:ALLUSERSPROFILE\Microsoft\Windows\Start Menu\Programs\CloneApp.lnk" $Shortcut -WorkingDirectory "$toolsPath"
+
+
 if(Test-Path 'HKCU:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers')
     {
 	Sleep 1
-      New-ItemProperty 'HKCU:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers' -Name $FileFullPath.FullName -Value 'RUNASADMIN' -PropertyType 'String' -Force
+      New-ItemProperty 'HKCU:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers' -Name $Shortcut.FullName -Value 'RUNASADMIN' -PropertyType 'String' -Force | Out-Null
     }
     else
     {
 	Sleep 1
-      New-Item 'HKCU:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers' -Force
-      New-ItemProperty 'HKCU:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers' -Name $FileFullPath.FullName -Value 'RUNASADMIN' -PropertyType 'String' -Force
+      New-Item 'HKCU:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers' -Force | Out-Null
+      New-ItemProperty 'HKCU:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers' -Name $Shortcut.FullName -Value 'RUNASADMIN' -PropertyType 'String' -Force | Out-Null
 	}
 
 if ((Get-WmiObject win32_operatingsystem).caption -like '*Server*')
     {
   if(Test-Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers')
     {
-	 New-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers' -Name $FileFullPath.FullName -Value 'DisableNXShowUI' -PropertyType 'String' -Force
+	 New-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers' -Name $Shortcut.FullName -Value 'DisableNXShowUI' -PropertyType 'String' -Force | Out-Null
     }
     else
     {
-	 New-Item 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers' -Force
-	 New-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers' -Name $FileFullPath.FullName -Value 'DisableNXShowUI' -PropertyType 'String' -Force
+	 New-Item 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers' -Force | Out-Null
+	 New-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers' -Name $Shortcut.FullName -Value 'DisableNXShowUI' -PropertyType 'String' -Force | Out-Null
 	}
 }
--- cloneapp/update.ps1
+++ cloneapp/update.ps1
@@ -1,24 +1,42 @@
 import-module au
 
+$au_Push = $false	
+$au_Force = $true
+
+
 $releases = 'http://www.mirinsoft.com/index.php/download/cloneapp/download/2-cloneapp/19-cloneapp'
 
 function global:au_SearchReplace {
-    @{
-        'tools\chocolateyInstall.ps1' = @{
-            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*packageName\s*=\s*)('.*')"  = "`$1'$($Latest.PackageName)'"
         }
-     }
+
+
+        ".\legal\VERIFICATION.txt" = @{
+          "(?i)(\s+x32:).*"            = "`${1} $($Latest.URL32)"
+          "(?i)(checksum32:).*"        = "`${1} $($Latest.Checksum32)"
+        }
+    }
 }
+
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge}
+
 
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
     $content = $download_page.Content
 
     $pattern = '(?<=</div>\ <!--\ end\ of\ screenshot\ zone\ -->)[\S\s]*Version: (?<Version>[\d\.]+)'
-    $url32 = 'http://www.mirinsoft.com/index.php/download/send/2-cloneapp/19-cloneapp'
+    $url = 'http://www.mirinsoft.com/index.php/download/send/2-cloneapp/19-cloneapp'
     $version = [regex]::Match($content, $pattern).groups['Version'].value
-    return @{ URL32 = $url32; Version = $version }
+
+     @{
+        URL32        = $url
+        FileType     = 'zip'
+        Version      = $version
+    }
 }
 
-update
+
+Update-Package -ChecksumFor none
