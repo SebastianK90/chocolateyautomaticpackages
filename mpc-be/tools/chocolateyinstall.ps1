128b9b711e74cbf24304d6a3fdfffeae09416a70
sippi90 <sippi90@users.noreply.github.com>
2019-04-28 22:46:58 +0200

    embedded mpc-be

--- mpc-be/tools/chocolateyinstall.ps1
+++ mpc-be/tools/chocolateyinstall.ps1
@@ -1,30 +1,22 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$packageName = 'mpc-be'
-$fileType    = 'EXE'
-$url32       = 'https://sourceforge.net/projects/mpcbe/files/MPC-BE/Release%20builds/1.5.2/MPC-BE.1.5.2.x86-installer.zip/download'
-$url64       = 'https://sourceforge.net/projects/mpcbe/files/MPC-BE/Release%20builds/1.5.2/MPC-BE.1.5.2.x64-installer.zip/download'
-$checksum32  = 'd5ed4d24ba9fa27647abc011b2dda4ee6feb01946595602aefa32a68d83f6c2d'
-$checksum64  = '6df7fbca20e21cb93c5835c0f59df3ad19400d824c887ab0453bd0b9186c9fe5'
-$toolsPath   = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-$silentArgs  = '/VERYSILENT /NORESTART /SUPPRESSMSGBOXES /SP-'
+$toolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition
+$silentArgs = '/VERYSILENT /NORESTART /SUPPRESSMSGBOXES /SP-'
+$fileType   = 'exe'
 
 $packageArgs = @{
-  packageName    = $packageName
-  url            = $url32
-  url64Bit       = $url64
-  checksum       = $checksum32
-  checksum64     = $checksum64
-  checksumType   = 'sha256'
-  checksumType64 = 'sha256'
-  unzipLocation  = $toolsPath
+  packageName = 'mpc-be'
+  file        = gi $toolsPath\*_x32.zip
+  file64      = gi $toolsPath\*_x64.zip
+  destination = $toolsPath
 }
 
-Install-ChocolateyZipPackage @packageArgs
+Get-ChocolateyUnzip @packageArgs
+Remove-Item $toolsPath\*.zip -ea 0
 
 $FileFullPath = Get-ChildItem $toolsPath -Recurse -Include *.exe | Sort-Object -Descending | Select-Object -First 1
- 
-Install-ChocolateyInstallPackage  $packageName $fileType $silentArgs $FileFullPath
+
+Install-ChocolateyInstallPackage -packageName 'mpc-be' $fileType $silentArgs $FileFullPath
 
 $regkey = Get-InstallRegistryKey "MPC-BE*"
 Install-ChocolateyPath $regkey.InstallLocation
--- mpc-be/update.ps1
+++ mpc-be/update.ps1
@@ -3,15 +3,23 @@ import-module au
 $releases = 'https://sourceforge.net/projects/mpcbe/files/MPC-BE/Release%20builds/'
 
 function global:au_SearchReplace {
-    @{
-        'tools\chocolateyInstall.ps1' = @{
-            "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
-            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*packageName\s*=\s*)('.*')"  = "`$1'$($Latest.PackageName)'"
         }
-     }
+
+
+        ".\legal\VERIFICATION.txt" = @{
+          "(?i)(\s+x32:).*"            = "`${1} $($Latest.URL32)"
+          "(?i)(checksum32:).*"        = "`${1} $($Latest.Checksum32)"
+          "(?i)(\s+x64:).*"            = "`${1} $($Latest.URL64)"
+          "(?i)(checksum64:).*"        = "`${1} $($Latest.Checksum64)"
+        }
+    }
 }
+
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge 'MPC-BE-installer'}
+
 
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
@@ -21,7 +29,13 @@ function global:au_GetLatest {
     $url32   = 'https://sourceforge.net'+$url +'MPC-BE.'+$version+ '.x86-installer.zip/download'
     $url64   = 'https://sourceforge.net'+$url +'MPC-BE.'+$version+ '.x64-installer.zip/download'
 
-    return @{ URL64 = $url64; URL32 = $url32; Version = $version }
+     @{
+        URL32        = $url32
+        URL64        = $url64
+        FileType     = 'zip'
+        Version      = $version
+    }
 }
 
-update -NoCheckUrl
+
+Update-Package -ChecksumFor none
