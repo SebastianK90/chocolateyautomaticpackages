686e5a6699e186dbe98920a0e7048ae64918c7a4
Chocolatey <chocolatey@realdimensions.net>
2019-08-26 12:06:51 +0000

    AU: 1 updated - teamspeak [skip ci] https://gist.github.com/a80d008be848e4754c459284f24be9fa/3c34cb66bafcb5aad0c9d0ab35305207b7eaf091

--- teamspeak/tools/chocolateyInstall.ps1
+++ teamspeak/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName = 'teamspeak'
-$url32 = 'https://files.teamspeak-services.com/releases/client/3.3.1/TeamSpeak3-Client-win32-3.3.1.exe'
-$url64 = 'https://files.teamspeak-services.com/releases/client/3.3.1/TeamSpeak3-Client-win64-3.3.1.exe'
-$checksum32  = '9960753c711e47c9b94da40c67cb8fbd74ce3137d0426b34ace26d8e48980091'
-$checksum64  = '3af5f4ad5ebc8e32a7fce48b73442182695a479247280746c7ffdcf029b3dcac'
+$url32 = 'https://files.teamspeak-services.com/releases/client/3.3.2/TeamSpeak3-Client-win32-3.3.2.exe'
+$url64 = 'https://files.teamspeak-services.com/releases/client/3.3.2/TeamSpeak3-Client-win64-3.3.2.exe'
+$checksum32  = '263ea40e29c2e67d708bd0e91d8aafff57036ea59371890b0c5dd6e7190441df'
+$checksum64  = 'acdd484629a3c4a140ae5963abc673f83ed2ea6f545cbebdadbd1b66bde6b02d'
 
 $packageArgs = @{
   packageName    = $packageName
