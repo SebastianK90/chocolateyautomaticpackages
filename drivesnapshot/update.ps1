c15feb0214ee4239c9dd4b77b5580e1a454ac83c
sippi90 <sippi90@users.noreply.github.com>
2019-08-29 18:54:55 +0200

    .

--- drivesnapshot/update.ps1
+++ drivesnapshot/update.ps1
@@ -16,8 +16,8 @@ function global:au_GetLatest {
   $url32   = 'http://www.drivesnapshot.de/download/snapshot.exe'
   $output32 = "$env:TEMP\snapshot.exe"
   $output64 = "$env:TEMP\snapshot64.exe"
-  Invoke-WebRequest -Uri $url32 -OutFile $output32 -UserAgent [Microsoft.PowerShell.Commands.PSUserAgent]::Chrome
-  Invoke-WebRequest -Uri $url64 -OutFile $output64 -UserAgent [Microsoft.PowerShell.Commands.PSUserAgent]::Chrome
+  Start-BitsTransfer -Source $url32 -Destination $output32 -TransferType Download
+  Start-BitsTransfer -Source $url64 -Destination $output64 -TransferType Download
   $ver = @(((Get-ChildItem $output32).VersionInfo).fileversion)
   $ver += @(((Get-ChildItem $output64).VersionInfo).fileversion)
   $version = ($ver | Sort-Object -Descending)[0]
