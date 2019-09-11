89c1da91f12846f1f1e11bb1fd5127380fbd742a
Chocolatey <chocolatey@realdimensions.net>
2019-09-11 12:06:34 +0000

    AU: 1 updated - drivesnapshot [skip ci] https://gist.github.com/a80d008be848e4754c459284f24be9fa/62caf61d4cbb3b2cfa09696f60ecb6450e8bf5e2

--- drivesnapshot/tools/chocolateyinstall.ps1
+++ drivesnapshot/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageName = 'drivesnapshot'
 $url32       = 'http://www.drivesnapshot.de/download/snapshot.exe'
 $url64       = 'http://www.drivesnapshot.de/download/snapshot64.exe'
-$checksum32  = '58a774171dbb4210d2fc62cb2401fc8e1d1bc267c782e716cfc2a1a9144cc939'
-$checksum64  = '21854ee23c51a43e1fcde41510d1029358935b622b181528a6216864fc89095b'
+$checksum32  = '1aa0ed2bcb5adc57f99b755dcae5ccd7cd83e06c0c6dc7b703fc2de8feda9555'
+$checksum64  = '02dd2c4955a24f26e2c1ea88d73f47c94d9ce88134530b79886e642e1d1ffcc2'
 $toolsPath   = (Split-Path $MyInvocation.MyCommand.Definition)
 $bits = Get-ProcessorBits
 $lic_path = "$env:USERPROFILE\drivesnapshot_lic"
