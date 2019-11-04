e02f2424403f01c8bb6538d7d677b42ffcc9c431
sippi90 <sippi90@users.noreply.github.com>
2019-11-04 18:53:52 +0100

    reverted, needs further testing...

--- cemu/tools/chocolateyinstall.ps1
+++ cemu/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageName = 'cemu'
 $url32       = 'http://cemu.info/releases/cemu_1.15.17.zip'
 $checksum32  = '317a34a15765e411d476464338d210eeca16b36c3e1401ea1f2096b0fa5a159f'
 $toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
-$cemu_folder =  "$(Get-ToolsLocation)\cemu"
+$cemu_folder = "$toolsPath\cemu"
 
 $packageArgs = @{
   packageName    = $packageName
@@ -22,4 +22,4 @@ Robocopy $latest $cemu_folder /R:0 /W:0 /E /XO
 
 Remove-Item $latest -Recurse -Force -ErrorAction SilentlyContinue
 
-Install-ChocolateyShortcut -shortcutFilePath "$env:ALLUSERSPROFILE\Microsoft\Windows\Start Menu\Programs\CEMU.lnk" "$cemu_folder\cemu.exe" -WorkingDirectory "$cemu_folder"
+Install-ChocolateyShortcut -shortcutFilePath "$env:ALLUSERSPROFILE\Microsoft\Windows\Start Menu\Programs\CEMU.lnk" "$cemu_folder\cemu.exe" -WorkingDirectory "$cemu_folder" -RunAsAdmin
