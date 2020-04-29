74f357a4268e5057de6210364f1da80ab58b5051
sippi90 <sippi90@users.noreply.github.com>
2020-04-29 17:47:48 +0200

    converted to embedded-package
    since downloading with choco-user agent is giving trouble. I've contacted the software vendor and he gave permission to create a embedded package.

--- EMDB/tools/chocolateyinstall.ps1
+++ EMDB/tools/chocolateyinstall.ps1
@@ -1,35 +1,18 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$packageName = 'emdb'
-$fileType    = 'EXE'
-$url32       = 'https://www.emdb.eu/bin/emdb.zip'
-$checksum32  = 'F94392D4F4D2C3F8A722F1FACD0BE9929A83F7024CDE36B5459CD87F74742762'
-$toolsPath   = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-$silentArgs  = '/VERYSILENT'
-$options =
-@{
-  Headers = @{
-    'GET' = 'https://www.emdb.eu/bin/emdb.zip HTTP/1.1'
-    'Accept' = 'text/html, application/xhtml+xml, image/jxr, */*'
-    'Referer'= 'https://www.emdb.eu/downloads.html'
-    'Accept-Language'= 'de-DE,de;q=0.5'
-    'User-Agent' = 'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko'
-    'Accept-Encoding' = 'gzip, deflate'
-    'Pragma' = 'no-cache'
-    'DNT' = '1'
-  }
-}
+$toolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition
+$silentArgs = '/VERYSILENT'
+$fileType   = 'exe'
 
 $packageArgs = @{
-  packageName    = $packageName
-  url            = $url32
-  checksum       = $checksum32
-  checksumType   = 'sha256'
-  unzipLocation  = $toolsPath
+  packageName = 'emdb'
+  file        = gi $toolsPath\*_x32.zip
+  destination = $toolsPath
 }
 
-Install-ChocolateyZipPackage @packageArgs -options $options
+Get-ChocolateyUnzip @packageArgs
+Remove-Item $toolsPath\*.zip -ea 0
 
-$FileFullPath = get-childitem $toolsPath -recurse -include *.exe | select -First 1
- 
-Install-ChocolateyInstallPackage  $packageName $fileType $silentArgs $FileFullPath
+$FileFullPath = Get-ChildItem $toolsPath -Recurse -Include *.exe | Sort-Object -Descending | Select-Object -First 1
+
+Install-ChocolateyInstallPackage -packageName 'emdb' $fileType $silentArgs $FileFullPath
--- EMDB/update.ps1
+++ EMDB/update.ps1
@@ -1,17 +1,14 @@
 ﻿import-module au
 
-# cd .\emdb
-
 $releases = 'https://www.emdb.eu/index.html'
 
 function global:au_SearchReplace {
     @{
-        'tools\chocolateyInstall.ps1' = @{
-            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-        }
+
      }
 }
+
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge }
 
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
@@ -19,9 +16,9 @@ function global:au_GetLatest {
     $pattern = '(?<=)[\S\s]*"version" content="(?<Version>[\d\.]+)'
 
     $version = [regex]::Match($content, $pattern).groups['Version'].value    
-    #$version = ($download_page.AllElements | where {$_.innerHTML -like "EMDB V*"} | Select-Object -ExpandProperty innerHTML -First 1).replace('EMDB V','')
+
     $url32   = 'https://www.emdb.eu/bin/emdb.zip'
     return @{ URL32 = $url32; Version = $version }
 }
 
-update
+Update-Package -ChecksumFor none
