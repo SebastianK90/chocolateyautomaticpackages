839ee2802fda8bc3f7121eba28822ab51ce5fcae
Chocolatey <chocolatey@realdimensions.net>
2020-05-14 00:08:02 +0000

    AU: 4 updated - telegram telegram.install telegram.portable WhatsApp [skip ci] https://gist.github.com/a80d008be848e4754c459284f24be9fa/16ba8a4f55a66c79b50df35394da6538a7a26e24

--- WhatsApp/tools/chocolateyinstall.ps1
+++ WhatsApp/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageName = 'whatsapp'
 $url32       = 'https://web.whatsapp.com/desktop/windows/release/ia32/WhatsAppSetup.exe'
 $url64       = 'https://web.whatsapp.com/desktop/windows/release/x64/WhatsAppSetup.exe'
-$checksum32  = 'fca43d921347faf0cfa5c160c8108932825f1103de30bf438ecc64f492e4f475'
-$checksum64  = 'ab840a8a4f578b201e3efd493d704586e0720f2cc28302bddb895c911b4f7052'
+$checksum32  = 'ce094a869eccccbb3e8fa72261bbe63f13d6715f91b7363fcd2d67c352bfc945'
+$checksum64  = 'ece62222419605ff607cb5bb68fe05a098d74320b9e2762cfc1c9dd42c31570b'
 
 $packageArgs = @{
   packageName            = $packageName
