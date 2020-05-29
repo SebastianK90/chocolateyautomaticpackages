15cde40d436498e62929ede081132a37aea9a196
sippi90 <sippi90@users.noreply.github.com>
2020-05-29 17:04:23 +0200

    converted to embedded package

--- gamesavemanager/tools/chocolateyinstall.ps1
+++ gamesavemanager/tools/chocolateyinstall.ps1
@@ -1,19 +1,19 @@
 ﻿$ErrorActionPreference = 'Stop'
- 
-$packageName = 'gamesavemanager'
-$url32       = 'https://www.gamesave-manager.com/?s=download&guid={C489235C-208E-4589-B35D-74DAC160CFED}&a=dl'
-$checksum32  = 'DE439BA7F5DF178D52A025F317D357D25EC01CAA6BF2CB468F9A005C9433AEC5'
-$toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
+
+$toolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
-  packageName    = $packageName
-  url            = $url32
-  checksum       = $checksum32
-  checksumType   = 'sha256'
-  unzipLocation  = $toolsPath
+  packageName = 'gamesavemanager'
+  file        = gi $toolsPath\*_x32.zip
+  destination = $toolsPath
 }
-Install-ChocolateyZipPackage @packageArgs
- 
+
+Get-ChocolateyUnzip @packageArgs
+
 $FileFullPath = get-childitem $toolsPath -Recurse -Filter "gs_mngr*" -include *.exe | select -First 1
-   
+
+Remove-Item $toolsPath\*.zip -ea 0
+
+$FileFullPath = Get-ChildItem $toolsPath -Recurse -Include *.exe | Sort-Object -Descending | Select-Object -First 1
+
 Install-ChocolateyShortcut -shortcutFilePath "$env:ALLUSERSPROFILE\Microsoft\Windows\Start Menu\Programs\GameSave Manager 3.lnk" $FileFullPath -WorkingDirectory "$toolsPath"
