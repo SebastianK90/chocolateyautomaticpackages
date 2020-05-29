b5b4265d2a7e158cc8ba2a1526760817bd508283
sippi90 <sippi90@users.noreply.github.com>
2020-05-29 11:11:59 +0200

    converted to embedded package

--- oldcalc/tools/chocolateyinstall.ps1
+++ oldcalc/tools/chocolateyinstall.ps1
@@ -1,33 +1,18 @@
 $ErrorActionPreference = 'Stop'
 
-$packageName = 'oldcalc'
-$fileType    = 'EXE'
-$url32       = 'https://winaero.com/e107_files/downloads/oldcalcwin10.zip'
-$checksum32  = '5AF17A5C9D65F95BF543D78C139740CF834A00644B255A6214A16576EB5CAB62'
-$toolsPath   = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-$silentArgs  = '/S'
-$options =
-@{
-  Headers = @{
-    'GET' = 'https://winaero.com/e107_files/downloads/oldcalcwin10.zip HTTP/1.1'
-    'Accept' = 'text/html, application/xhtml+xml, image/jxr, */*'
-    'Referer' = 'https://winaero.com/download.php?view.1795';
-    'Accept-Language' = 'de-DE,de;q=0.5'
-    'User-Agent' = 'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko';
-    'Accept-Encoding' = 'gzip, deflate'
-  }
-}
+$toolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition
+$silentArgs = '/S'
+$fileType   = 'exe'
 
 $packageArgs = @{
-  packageName    = $packageName
-  url            = $url32
-  checksum       = $checksum32
-  checksumType   = 'sha256'
-  unzipLocation  = $toolsPath
+  packageName = 'oldcalc'
+  file        = gi $toolsPath\*_x32.zip
+  destination = $toolsPath
 }
 
-Install-ChocolateyZipPackage @packageArgs -options $options
+Get-ChocolateyUnzip @packageArgs
+Remove-Item $toolsPath\*.zip -ea 0
 
-$FileFullPath = get-childitem $toolsPath -recurse -include *.exe | select -First 1
- 
-Install-ChocolateyInstallPackage  $packageName $fileType $silentArgs $FileFullPath
+$FileFullPath = Get-ChildItem $toolsPath -Recurse -Include *.exe | Sort-Object -Descending | Select-Object -First 1
+
+Install-ChocolateyInstallPackage -packageName 'oldcalc' $fileType $silentArgs $FileFullPath
