c32946698443ab8853773bf0cb16d7d61f7387d4
sippi90 <sippi90@users.noreply.github.com>
2020-09-15 17:58:46 +0200

    .

--- coretemp/tools/chocolateyinstall.ps1
+++ coretemp/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'coretemp'
-$url32       = 'https://www.alcpu.com/CoreTemp/CoreTemp64.zip'
-$url64       = 'https://www.alcpu.com/CoreTemp/CoreTemp32.zip'
+$url32       = 'https://www.alcpu.com/CoreTemp/CoreTemp32.zip'
+$url64       = 'https://www.alcpu.com/CoreTemp/CoreTemp64.zip'
 $checksum32  = 'b7921d20c2387aa7eb7a5a25a33cd045024cce996d05378165be48c267b68150'
 $checksum64  = '5f4609203338ba6439b59529295d5d449bc53581eeac28678f9b47596e3c094a'
 $toolsPath   = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
