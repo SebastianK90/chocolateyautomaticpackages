9ea85a032f13f7dcfc5a07476abbb7a05f7ef34d
sippi90 <sippi90@users.noreply.github.com>
2020-09-18 14:52:33 +0200

    fixed messed up checksums

--- coretemp/tools/chocolateyinstall.ps1
+++ coretemp/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageName = 'coretemp'
 $url32       = 'https://www.alcpu.com/CoreTemp/CoreTemp32.zip'
 $url64       = 'https://www.alcpu.com/CoreTemp/CoreTemp64.zip'
-$checksum32  = 'b7921d20c2387aa7eb7a5a25a33cd045024cce996d05378165be48c267b68150'
-$checksum64  = '5f4609203338ba6439b59529295d5d449bc53581eeac28678f9b47596e3c094a'
+$checksum32  = '5F4609203338BA6439B59529295D5D449BC53581EEAC28678F9B47596E3C094A'
+$checksum64  = 'B7921D20C2387AA7EB7A5A25A33CD045024CCE996D05378165BE48C267B68150'
 $toolsPath   = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 
 $packageArgs = @{
