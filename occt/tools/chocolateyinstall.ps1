e10048b2512bed862e23dca399f4e26526239b49
Chocolatey <chocolatey@realdimensions.net>
2020-10-22 00:06:48 +0000

    AU: 1 updated - occt [skip ci] https://gist.github.com/a80d008be848e4754c459284f24be9fa/7375a6c47b1d36dff4e984cc756fe4c13b062899

--- occt/tools/chocolateyinstall.ps1
+++ occt/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
  
 $packageName = 'occt'
 $url32       = 'https://www.ocbase.com/download'
-$checksum32  = '50a73e23ed8dc29543d567cd27a0ef60fa3f3a3adce9ac97aad1005bfbff073a'
+$checksum32  = '5cdb48fb1180f996b7aa1e09086b308a3f499b41c2569ef39f0d48aaa8a4c7a6'
 $toolsPath   = (Split-Path $MyInvocation.MyCommand.Definition)
 
  
