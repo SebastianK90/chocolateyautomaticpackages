2feea3ba9539b01fd2fe42f7ea3e502ee60414ca
sippi90 <kowatsch.sebastian@outlook.de>
2022-01-11 22:34:59 +0100

    Update update.ps1

--- gamesavemanager/update.ps1
+++ gamesavemanager/update.ps1
@@ -11,9 +11,9 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-    $a = $download_page.Links | Where-Object {$_.OuterHTML -like '*Download GameSave*'}
+    $a = $download_page.Links | Where-Object {$_.OuterHTML -like '*Download*'}
     $r = $a.href  -replace '&amp;','&'
-    $url32 = ($a.href -replace '&amp;','&') + "&a=dl"
+    $url32 = (($a.href -replace '&amp;','&') + "&a=dl")[0]
     [string]$version =  [regex]::match($a.title,'[0-9]+(\.[0-9]+)*').value
 
     $headers = @{
