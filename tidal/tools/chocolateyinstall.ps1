6f1e65ea766e47b59b970580a463602a836526df
Chocolatey <chocolatey@realdimensions.net>
2022-01-26 12:06:28 +0000

    AU: 1 updated - tidal [skip ci] https://gist.github.com/a80d008be848e4754c459284f24be9fa/5c56e273be0f4f9a3bb6c55cdee22819e5d01c9b

--- tidal/tools/chocolateyinstall.ps1
+++ tidal/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName = 'tidal'
 $url32       = 'http://download.tidal.com/desktop/TIDALSetup.exe'
-$checksum32  = '2569878529d16a0bf53910ee581ae0dd64aeee83525a05dbdd9271c74336bf7d'
+$checksum32  = '020685eeacf1857c34d6f034b94616e4ff45634eeb95bb720be667ed364de7ff'
 
 $packageArgs = @{
   packageName            = $packageName
