2d28c207b3f23025b8ea8b8c47b40a5144bee593
Chocolatey <chocolatey@realdimensions.net>
2022-02-28 00:06:43 +0000

    AU: 1 updated - makemkv [skip ci] https://gist.github.com/a80d008be848e4754c459284f24be9fa/c32bb3b299c271288cce9d24e82f33e42601c30b

--- makemkv/tools/chocolateyInstall.ps1
+++ makemkv/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'MakeMKV'
-$url32       = 'https://www.makemkv.com/download/Setup_MakeMKV_v1.16.5.exe'
-$checksum32  = 'c972874e8a79aafd908c2f67e3928527fd01e31a6f3ba61ce40b615a654fb984'
+$url32       = 'https://www.makemkv.com/download/Setup_MakeMKV_v1.16.7.exe'
+$checksum32  = 'df6e291216fe8926e760650e11cc794def8dde3462aca8f68e6166e8c9227c80'
 $toolsPath   = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 
 $packageArgs = @{
