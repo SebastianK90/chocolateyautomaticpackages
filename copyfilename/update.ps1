552802faa892d65e333cfb6dac285e923a8e29c4
sippi90 <kowatsch.sebastian@outlook.de>
2022-12-04 00:27:30 +0100

    Update update.ps1

--- copyfilename/update.ps1
+++ copyfilename/update.ps1
@@ -19,8 +19,7 @@ function global:au_GetLatest {
     $re      = 'copyfilename.*.exe'
     $url     = $download_page.links | ? href -match $re | select -First 1 -expand href
     $url32   = 'http://www.bullzip.com/' + $url
-    $version = (($url.Substring(26)).replace('.exe','') -replace '_','.')
-
+    $version = (((($url -split '.exe')[0]).Substring(26)).replace('.exe','') -replace '_','.')
     return @{ URL32 = $url32; Version = $version }
 }
 
