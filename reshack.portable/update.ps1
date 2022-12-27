73ede3db5e5125477b884dd5ad0e1444927f3023
sippi90 <kowatsch.sebastian@outlook.de>
2022-12-27 16:58:35 +0100

    Update update.ps1

--- reshack.portable/update.ps1
+++ reshack.portable/update.ps1
@@ -12,14 +12,22 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-    $url_down = Invoke-WebRequest -Uri $releases -UseBasicParsing
-    $download_page = $url_down.Content
-    #$download_page.AllElements | ? tagName -eq 'strong' | ? InnerText -match '^Version' | % InnerText | set version
-    #$version = $version -split ' ' | select -Last 1
-    $pattern = '(?<=<h2>Resource\ Hacker)[\S\s]*<strong>Version (?<Version>[\d\.]+)'
-    $version = [regex]::Match($download_page, $pattern).groups['Version'].value
-    $url =  $url_down.links | ? href -match 'zip' | % href | Select -First 1
-    $url32 = "$releases/$url"
+
+        
+    $useragents = @('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246',`
+    'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3112 Safari/537.36',`
+    'Mozilla/5.0 (Windows NT x.y; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0')
+    $random = Get-Random -Maximum 3
+    
+
+    $url32   = 'http://www.angusj.com/resourcehacker/resource_hacker.zip'
+    $output = "$env:TEMP\resource_hacker.zip"
+    Invoke-WebRequest -Uri $url32 -OutFile $output -UserAgent [$useragents[$random]]
+    Expand-Archive -Path $env:TEMP\resource_hacker.zip -DestinationPath $env:TEMP\resource_hacker -Force
+    $ver = (Get-ChildItem $env:TEMP\resource_hacker\ResourceHacker.exe).VersionInfo
+    $version = $ver.FileVersion
+    Remove-Item $output,$env:TEMP\resource_hacker -Recurse -ea 0
+
     
     @{ URL32 = $url32; Version = $version }
 }
