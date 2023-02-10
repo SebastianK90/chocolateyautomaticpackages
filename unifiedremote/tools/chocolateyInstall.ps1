c9f29711a6260e9f27d98e6cd8e41ce3e337e219
Chocolatey <chocolatey@realdimensions.net>
2023-02-10 18:07:55 +0000

    AU: 2 updated - mpc-be unifiedremote [skip ci] https://gist.github.com/a80d008be848e4754c459284f24be9fa/2302d01e3ad878eff1d50f6a3e7c8944f8faa33a

--- unifiedremote/tools/chocolateyInstall.ps1
+++ unifiedremote/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
  
 $packageName = 'unifiedremote'
 $url32       = 'https://www.unifiedremote.com/download/windows'
-$checksum32  = '6fe9b9e8a32e81b40a00b36adea705d766aab7fa7eb6018e79ac07a327af695e'
+$checksum32  = 'e084d1a666d9bbfdc7bdc2be24e09b9b93edcf6ab14586a3aab74e74bd87a37d'
 $toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
 $packageArgs = @{
   packageName            = $packageName
