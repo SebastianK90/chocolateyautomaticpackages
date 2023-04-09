ebe22df671dd2e31ab23122613e26d5a61b74d85
SebastianK90 <12467271+SebastianK90@users.noreply.github.com>
2023-04-09 12:43:52 +0200

    removed after 5.0.3 migration

--- paint.net.portable/tools/chocolateyBeforeModiy.ps1
+++ paint.net.portable/tools/chocolateyBeforeModiy.ps1
@@ -4,6 +4,3 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 
 Get-Process paintdotnet -ErrorAction SilentlyContinue | Stop-Process -ErrorAction SilentlyContinue
 Remove-Item $toolsPath\*.zip -ErrorAction SilentlyContinue
-
-#for cleaning up / migration to C:\tools - will be removed after this version 5.0.3
-Remove-Item -Path C:\ProgramData\chocolatey\lib\paint.net.portable\tools -Exclude '*.ps1' -Recurse
--- paint.net.portable/tools/chocolateyInstall.ps1
+++ paint.net.portable/tools/chocolateyInstall.ps1
@@ -12,7 +12,3 @@ Get-ChocolateyUnzip @packageArgs
 
 $Shortcut = Get-ChildItem $packageArgs['Destination'] -Recurse -Include paintdotnet.exe | Select-Object -First 1
 Install-ChocolateyShortcut -shortcutFilePath "$env:ALLUSERSPROFILE\Microsoft\Windows\Start Menu\Programs\Paint.Net.lnk" $Shortcut -WorkingDirectory $packageArgs['Destination']
-
-#for cleaning up / migration to C:\tools - will be removed after this version 5.0.3
-Get-Process paintdotnet -ErrorAction SilentlyContinue | Stop-Process -ErrorAction SilentlyContinue
-Remove-Item -Path $toolsPath -Exclude '*.ps1' -Recurse
