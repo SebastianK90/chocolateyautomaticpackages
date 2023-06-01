67750c3e8b0eab18231c67531fe5ac2c048c96c4
Chocolatey <chocolatey@realdimensions.net>
2023-06-01 00:08:12 +0000

    AU: 1 updated - occt [skip ci] https://gist.github.com/SebastianK90/a80d008be848e4754c459284f24be9fa/51c46c37a598f9041fdf973fb535696ad023e9a1

--- occt/tools/chocolateyinstall.ps1
+++ occt/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
  
 $packageName = 'occt'
 $url32       = 'https://www.ocbase.com/download/edition:Personal'
-$checksum32  = '231f3a03e8ce870602b71f9d83f6c8d20f3a6a8e74f91b78f72dd7fd080451e6'
+$checksum32  = '84e89c204eade8baf561982a0cfb7cc8cb96c1a2688619e7ee51015031001359'
 $toolsPath   = (Split-Path $MyInvocation.MyCommand.Definition)
 
  
