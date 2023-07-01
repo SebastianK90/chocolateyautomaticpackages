c2fed168d4109abb41b9d0fa149d022855f7b7aa
SebastianK90 <12467271+SebastianK90@users.noreply.github.com>
2023-07-01 20:27:09 +0200

    Update update.ps1

--- teamspeak/update.ps1
+++ teamspeak/update.ps1
@@ -1,6 +1,6 @@
 import-module au
 
-$releases = 'https://www.teamspeak.com/en/downloads/'
+$releases = 'https://files.teamspeak-services.com/releases/client/'
 
 function global:au_SearchReplace {
     @{
@@ -15,8 +15,13 @@ function global:au_SearchReplace {
 
 function global:au_GetLatest {
   $downloads = Invoke-WebRequest -Uri $releases -UseBasicParsing
-  $url32 =  ($downloads.links | Where-Object {$_.outerHTML -like '*client-win32*'}).href
-  $url64 =  ($downloads.links | Where-Object {$_.outerHTML -like '*client-win64*'}).href
+  
+  $ver = ($downloads.Links | Where-Object {$_.href} | Select-Object -Last 1).href
+  $link = "https://files.teamspeak-services.com/releases/client/$($ver)/"
+  $l = Invoke-WebRequest -Uri $link -UseBasicParsing
+  
+  $url32 = $link + ($l.links | Where-Object {$_.outerHTML -like '*client-win32*'}).href
+  $url64 = $link + ($l.links | Where-Object {$_.outerHTML -like '*client-win64*'}).href
   $version = [regex]::match($url64,'[0-9]+(\.[0-9]+)*').value
   
   return @{ URL64 = $url64; URL32 = $url32; Version = $version }
