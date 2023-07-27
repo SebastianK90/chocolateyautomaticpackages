3d46e92586dbac1f140205d8f409a5d027cd50e1
SebastianK90 <12467271+SebastianK90@users.noreply.github.com>
2023-07-27 18:14:05 +0200

    Update update.ps1

--- pcloud/update.ps1
+++ pcloud/update.ps1
@@ -1,4 +1,4 @@
-﻿import-module au
+import-module au
 
 $releases = 'https://www.pcloud.com/de/release-notes/windows.html'
 function global:au_SearchReplace {
@@ -13,15 +13,19 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing -UserAgent 'chocolatey'
-  $html = new-object -ComObject HTMLFile
-  $html.IHTMLDocument2_write($download_page.rawcontent)
-  $dirty_ver = (($html.getElementsByTagName('div') | Where-Object {$_.innerText -like '*| Download'}).textcontent | Select-Object -First 1)
-  $url32 = 'https://partner.pcloud.com/dl/win'
-  $url64 = 'https://partner.pcloud.com/dl/win64'
-  $version =  [regex]::match($dirty_ver,'[0-9]+(\.[0-9]+)*').value
-  
-  return @{URL64 = $url64; URL32 = $url32; Version = $version }
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $html = new-object -ComObject HTMLFile
+    $html.IHTMLDocument2_write($download_page.rawcontent)
+    $ver = (($html.getElementsByTagName('div') | Where-Object {$_.innerText -like '*| Download'}) | Select-Object -ExpandProperty textcontent -First 1)
+    $version =  [regex]::match($ver,'[0-9]+(\.[0-9]+)*').value
+    $url32 = 'https://partner.pcloud.com/dl/win'
+    $url64 = 'https://partner.pcloud.com/dl/win64'
+
+     @{
+        URL32        = $url32
+        URL64        = $url64
+        Version      = $version
+    }
 }
 
 update
