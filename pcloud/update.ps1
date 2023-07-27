6c7c70784d812a725162c405dc50a1f87a25261a
SebastianK90 <12467271+SebastianK90@users.noreply.github.com>
2023-07-27 08:57:46 +0200

    Update update.ps1

--- pcloud/update.ps1
+++ pcloud/update.ps1
@@ -1,4 +1,7 @@
-﻿import-module au
+﻿[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls, [Net.SecurityProtocolType]::Tls11, [Net.SecurityProtocolType]::Tls12, [Net.SecurityProtocolType]::Ssl3
+[Net.ServicePointManager]::SecurityProtocol = "Tls, Tls11, Tls12, Ssl3"
+
+import-module au
 
 function global:au_SearchReplace {
     @{
