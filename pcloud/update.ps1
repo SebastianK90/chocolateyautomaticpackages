72ecf86d7338c635e665dedb49cacd13cb99edd6
SebastianK90 <12467271+SebastianK90@users.noreply.github.com>
2023-07-27 08:54:09 +0200

    initial release

---
+++ pcloud/update.ps1
@@ -0,0 +1,27 @@
+﻿import-module au
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+     }
+}
+
+function global:au_GetLatest {
+
+    $url32 = 'https://partner.pcloud.com/dl/win'
+    $url64 = 'https://partner.pcloud.com/dl/win64'
+    $output = "$env:TEMP\pcloud.exe"
+    Invoke-WebRequest -Uri $url32 -OutFile $output -UseBasicParsing
+    $version = ((Get-ChildItem $output).VersionInfo).ProductVersion
+    Remove-Item $output
+   
+    
+    return @{URL64 = $url64; URL32 = $url32; Version = $version }
+}
+
+update
