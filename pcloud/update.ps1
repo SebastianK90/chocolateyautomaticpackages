f1530bf8a1500256fd785deba5b3ad435b3e2b9c
SebastianK90 <12467271+SebastianK90@users.noreply.github.com>
2023-07-27 17:31:31 +0200

    Update update.ps1

--- pcloud/update.ps1
+++ pcloud/update.ps1
@@ -1,65 +1,6 @@
 ﻿import-module au
 
-#region functions
-<#
-.SYNOPSIS
-    Aquire the url that is being redirected to when using the passed url parameter
-
-.DESCRIPTION
-    When a website only supplies a url to the latest version, but that url is redirected
-    to a different url to aquire the actual binary file.
-    Then this function can be used to simplify that aquiral.
-
-.PARAMETER url
-    The url to check for redirection
-
-.PARAMETER referer
-    An optional parameter to use when a website requires the referer header to
-    be used
-
-.OUTPUTS
-    The redirected url when one is found, otherwise returns the same url that was passed.
-
-.LINK
-    https://wormiecorp.github.io/Wormies-AU-Helpers/docs/functions/get-redirectedurl
-
-#>
-
-function Get-RedirectedUrl {
-    param(
-        [Parameter(Mandatory = $true)]
-        [uri]$url,
-        [uri]$referer,
-        [Alias('DisableEscape','RawUrl')]
-        [switch]$NoEscape
-    )
-
-    $req = [System.Net.WebRequest]::CreateDefault($url)
-    if ($referer) {
-        $req.Referer = $referer
-
-    }
-    $resp = $req.GetResponse()
-
-    if ($resp -and $resp.ResponseUri.OriginalString -ne $url) {
-        Write-Verbose "Found redirected url '$($resp.ResponseUri)"
-        if ($NoEscape -or $($resp.ResponseUri.OriginalString) -match '\%\d+' ) {
-            $result = $resp.ResponseUri.OriginalString
-        }
-        else {
-            $result = [uri]::EscapeUriString($resp.ResponseUri.OriginalString)
-        }
-    }
-    else {
-        Write-Warning "No redirected url was found, returning given url."
-        $result = $url
-    }
-
-    $resp.Dispose()
-
-    return $result
-}
-#endregion functions
+$releases = 'https://www.pcloud.com/de/release-notes/windows.html'
 
 function global:au_SearchReplace {
     @{
@@ -73,10 +14,12 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -Uri $releases -UserAgent 'chocolatey'
+  $dirty_ver = (($download_page.ParsedHtml.getElementsByTagName("div") | ? {$_.innerText -like '*| Download'}).textcontent | Select-Object -First 1)
   $url32 = 'https://partner.pcloud.com/dl/win'
   $url64 = 'https://partner.pcloud.com/dl/win64'
-  $version =  [regex]::match(((Get-RedirectedUrl -url $url64) -split '\/')[-1],'[0-9]+(\.[0-9]+)*').value
-   
+  $version =  [regex]::match($dirty_ver,'[0-9]+(\.[0-9]+)*').value
+  
   return @{URL64 = $url64; URL32 = $url32; Version = $version }
 }
 
