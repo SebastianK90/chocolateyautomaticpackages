7375f8bc1b05b6800cd08f7eabb97e5e0fded7b7
SebastianK90 <12467271+SebastianK90@users.noreply.github.com>
2023-07-29 22:03:23 +0200

    Update update.ps1

--- pcloud/update.ps1
+++ pcloud/update.ps1
@@ -12,19 +12,29 @@ function global:au_SearchReplace {
 
 function global:au_GetLatest {
 
-    $useragents = @('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246',`
+  $useragents = @('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246',`
     'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3112 Safari/537.36',`
-    'Mozilla/5.0 (Windows NT x.y; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0')
-    $random = Get-Random -Maximum 3
+  'Mozilla/5.0 (Windows NT x.y; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0')
+  $random = Get-Random -Maximum 3
     
-    $url32 = 'https://partner.pcloud.com/dl/win'
-    $url64 = 'https://partner.pcloud.com/dl/win64'
-    $output = "$env:TEMP\pcloud.exe"
-    Invoke-WebRequest -Uri $url32 -OutFile $output -UserAgent [$useragents[$random]]
-    $version = ((Get-ChildItem $output).VersionInfo).fileversion
-    Remove-Item $output
+  $url32 = 'https://partner.pcloud.com/dl/win'
+  $url64 = 'https://partner.pcloud.com/dl/win64'
+  $output = "$env:TEMP\pcloud.exe"
+  [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+  Invoke-WebRequest -Uri $url32 -OutFile $output -UserAgent [$useragents[$random]]
+  $version = ((Get-ChildItem $output).VersionInfo).fileversion
+  Remove-Item $output
     
-    return @{ URL64 = $url64; URL32 = $url32; Version = $version }
+  return @{ URL64 = $url64; URL32 = $url32; Version = $version }
 }
 
 update
+
+
+[ServicePointManager.Expect100Continue] = true
+
+try {
+  if ([Net.ServicePointManager]::SecurityProtocol -notcontains 'Tls12') {
+    [Net.ServicePointManager]::SecurityProtocol += [Net.SecurityProtocolType]::Tls12
+    }
+  }
