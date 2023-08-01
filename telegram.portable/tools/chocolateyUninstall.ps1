0c542ff2a41828e1569df091e374e86d153c6161
SebastianK90 <12467271+SebastianK90@users.noreply.github.com>
2023-08-02 01:37:57 +0200

    Update chocolateyUninstall.ps1

--- telegram.portable/tools/chocolateyUninstall.ps1
+++ telegram.portable/tools/chocolateyUninstall.ps1
@@ -1,5 +1,6 @@
 $ErrorActionPreference = 'Stop'
 
+Get-Process telegram -ErrorAction SilentlyContinue | Stop-Process -ErrorAction SilentlyContinue
 $icon_name = (Get-ChildItem "$env:ALLUSERSPROFILE\Microsoft\Windows\Start Menu\Programs" -Filter "Telegram.lnk" -ErrorAction SilentlyContinue).FullName
 Remove-Item C:\tools\telegram.portable -Recurse -Force -ErrorAction SilentlyContinue
 Remove-Item $icon_name -ErrorAction SilentlyContinue
