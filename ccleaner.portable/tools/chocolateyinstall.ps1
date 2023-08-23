111a22b5743bc45e548060be8950adfc91e70f0e
Chocolatey <chocolatey@realdimensions.net>
2023-08-23 18:06:42 +0000

    AU: 1 updated - ccleaner.portable [skip ci] https://gist.github.com/SebastianK90/a80d008be848e4754c459284f24be9fa/303e6a0a37865b6eb89b9279772a8f25f92c203d

--- ccleaner.portable/tools/chocolateyinstall.ps1
+++ ccleaner.portable/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'ccleaner.portable'
-$url32       = 'https://download.ccleaner.com/portable/ccsetup614.zip'
-$checksum32  = 'be9900141e1fa8b4236138dd06307c2806b3b90c7e082e3a25c4c4de02417427'
+$url32       = 'https://download.ccleaner.com/portable/ccsetup615.zip'
+$checksum32  = '267e5789927d1f2abb08b4e27e8ee988e7888cc3ec7f9887e9b107ea191cc0e2'
 $toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
 $bits = Get-ProcessorBits
 $ccleaner_temp = "$env:USERPROFILE\ccleaner_temp"
