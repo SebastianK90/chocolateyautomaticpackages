28e433c60a4fb2d6970627e524376ad2ff57d7fd
Chocolatey <chocolatey@realdimensions.net>
2023-09-13 18:07:10 +0000

    AU: 4 updated - ccleaner.portable telegram telegram.install telegram.portable [skip ci] https://gist.github.com/SebastianK90/a80d008be848e4754c459284f24be9fa/e53f88429146ffecbd491c4f177e14c912580761

--- ccleaner.portable/tools/chocolateyinstall.ps1
+++ ccleaner.portable/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'ccleaner.portable'
-$url32       = 'https://download.ccleaner.com/portable/ccsetup615.zip'
-$checksum32  = '267e5789927d1f2abb08b4e27e8ee988e7888cc3ec7f9887e9b107ea191cc0e2'
+$url32       = 'https://download.ccleaner.com/portable/ccsetup616.zip'
+$checksum32  = 'b6a8950f5fae31a0f8d6d49f306bcd3f8e3b7980fcedebf8622653c26586065a'
 $toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
 $bits = Get-ProcessorBits
 $ccleaner_temp = "$env:USERPROFILE\ccleaner_temp"
