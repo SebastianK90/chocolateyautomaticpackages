382c0c27e0f79a208b5cfbeaa0feead2c90d3bee
Chocolatey <chocolatey@realdimensions.net>
2023-10-31 00:06:56 +0000

    AU: 2 updated - EMDB reshack.portable [skip ci] https://gist.github.com/SebastianK90/a80d008be848e4754c459284f24be9fa/f0a48298cfb60491c705abaa45dfc9d25d3861a1

--- reshack.portable/tools/chocolateyInstall.ps1
+++ reshack.portable/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName = 'reshack.portable'
 $url32       = 'http://www.angusj.com/resourcehacker/resource_hacker.zip'
-$checksum32  = '0b3203a2a866a7d1957f2bce8c2346a1dcd8b02c86c895fbc295d89c34dccba6'
+$checksum32  = '8e56a5c84999f036355759e5cb759e3055df761b55d9ae6a72825d4199f328e9'
 $toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
