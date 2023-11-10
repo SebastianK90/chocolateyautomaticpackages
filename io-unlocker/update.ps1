25c6eea53634ab6c8fbd7e822bf7750e18acde9e
SebastianK90 <12467271+SebastianK90@users.noreply.github.com>
2023-11-10 23:45:00 +0100

    changed update script to fileversion

--- io-unlocker/update.ps1
+++ io-unlocker/update.ps1
@@ -1,24 +1,24 @@
 import-module au
 
-$releases = 'http://www.iobit.com/en/iobit-unlocker.php'
+$url32 = 'https://cdn.iobit.com/dl/unlocker-setup.exe'
 
 function global:au_SearchReplace {
-    @{
-        'tools\chocolateyInstall.ps1' = @{
-            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-        }
-     }
+  @{
+    'tools\chocolateyInstall.ps1' = @{
+      "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+      "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+    }
+  }
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases
-    $content = $download_page.Content
-
-    $pattern = '(?<=button\ btn-jadegreen\ large)[\S\s]*"ver_size"><span>V (?<Version>[\d\.]+)'
-    $url32   = 'https://cdn.iobit.com/dl/unlocker-setup.exe'
-    $version = [regex]::Match($content, $pattern).groups['Version'].value
-    return @{ URL32 = $url32; Version = $version }
+    
+  $output32 = "$env:TEMP\unlocker-setup.exe"
+  Start-BitsTransfer -Source $url32 -Destination $output32 -TransferType Download
+  $version = @(((Get-ChildItem $output32).VersionInfo).fileversion)
+  Remove-Item $output32
+  
+  return @{ URL32 = $url32; Version = $version }
 }
 
 update
