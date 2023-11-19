31d0b9851a02b653471474a4aef34e0b9a90e4c1
SebastianK90 <12467271+SebastianK90@users.noreply.github.com>
2023-11-19 23:23:23 +0100

    fixed icon url + fixed update.ps1

--- io-unlocker/update.ps1
+++ io-unlocker/update.ps1
@@ -15,7 +15,7 @@ function global:au_GetLatest {
     
   $output32 = "$env:TEMP\unlocker-setup.exe"
   Start-BitsTransfer -Source $url32 -Destination $output32 -TransferType Download
-  $version = @(((Get-ChildItem $output32).VersionInfo).fileversion)
+  $version = ((Get-ChildItem $output32).VersionInfo).fileversion
   Remove-Item $output32
   
   return @{ URL32 = $url32; Version = $version }
