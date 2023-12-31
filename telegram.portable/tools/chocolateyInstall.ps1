9a756be90335f112ca59bf5eef480b0415f65be2
Chocolatey <chocolatey@realdimensions.net>
2023-12-31 18:07:05 +0000

    AU: 5 updated - ChanSort ImDisk-Toolkit telegram telegram.install telegram.portable [skip ci] https://gist.github.com/SebastianK90/a80d008be848e4754c459284f24be9fa/f3b9aae27022b8a4b6524387d2fa0d3bd4d9ea34

--- telegram.portable/tools/chocolateyInstall.ps1
+++ telegram.portable/tools/chocolateyInstall.ps1
@@ -4,8 +4,8 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
   PackageName    = 'telegram.portable'
-  FileFullPath   = "$toolsPath\tportable.4.13.1_x32.zip"
-  FileFullPath64 = "$toolsPath\tportable-x64.4.13.1_x64.zip"
+  FileFullPath   = "$toolsPath\tportable.4.14.0_x32.zip"
+  FileFullPath64 = "$toolsPath\tportable-x64.4.14.0_x64.zip"
   Destination    = "C:\tools\telegram.portable"
 }
 
