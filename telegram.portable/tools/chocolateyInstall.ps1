0ebff40b6cd8af9b909f895c732e3397be9502d2
Chocolatey <chocolatey@realdimensions.net>
2024-01-20 00:07:24 +0000

    AU: 3 updated - telegram telegram.install telegram.portable [skip ci] https://gist.github.com/SebastianK90/a80d008be848e4754c459284f24be9fa/0bb2deb79d14d71327b0ab365c5e3493568eba13

--- telegram.portable/tools/chocolateyInstall.ps1
+++ telegram.portable/tools/chocolateyInstall.ps1
@@ -4,8 +4,8 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
   PackageName    = 'telegram.portable'
-  FileFullPath   = "$toolsPath\tportable.4.14.8_x32.zip"
-  FileFullPath64 = "$toolsPath\tportable-x64.4.14.8_x64.zip"
+  FileFullPath   = "$toolsPath\tportable.4.14.9_x32.zip"
+  FileFullPath64 = "$toolsPath\tportable-x64.4.14.9_x64.zip"
   Destination    = "C:\tools\telegram.portable"
 }
 
