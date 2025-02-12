907c4ae28c037ba0aea339efdad3ecb6ee588da6
SebastianK90 <12467271+SebastianK90@users.noreply.github.com>
2025-02-12 18:48:31 +0100

    .

--- paint.net.portable/tools/chocolateyInstall.ps1
+++ paint.net.portable/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
   PackageName    = 'paint.net.portable'
-  FileFullPath   = "$toolsPath\paint.net.5.0.13.portable.x64_x64.zip"
+  FileFullPath   =  Get-Item "$toolsPath\paint.net*.portable.x64_x64.zip"
   Destination    = "C:\tools\paint.net.portable\"
 }
 
