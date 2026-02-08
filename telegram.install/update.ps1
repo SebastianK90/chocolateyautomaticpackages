042f4f87dc9bb19f4a014823eaa58c5d2fd829ea
SebastianK90 <12467271+SebastianK90@users.noreply.github.com>
2026-02-08 19:30:12 +0100

    .

--- telegram.install/update.ps1
+++ telegram.install/update.ps1
@@ -1,47 +1,39 @@
 ﻿Import-Module Chocolatey-AU
 
-$releases = 'https://github.com/telegramdesktop/tdesktop/releases/latest'
-
+# Regex für Standard URL/Checksum Ersetzung in chocolateyInstall.ps1
 function global:au_SearchReplace {
-   @{
-        ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*packageName\s*=\s*)('.*')"  = "`$1'$($Latest.PackageName)'"
-            "(?i)(^\s*FileFullPath64\s*=\s*`"[$]toolsPath\\).*" = "`${1}$($Latest.FileName64)`""
-            "(?i)(^\s*FileFullPath\s*=\s*`"[$]toolsPath\\).*" = "`${1}$($Latest.FileName32)`""
-        }
-
-
+    @{
         ".\legal\VERIFICATION.txt" = @{
-          "(?i)(\s+x32:).*"            = "`${1} $($Latest.URL32)"
-          "(?i)(checksum32:).*"        = "`${1} $($Latest.Checksum32)"
-          "(?i)(\s+x64:).*"            = "`${1} $($Latest.URL64)"
-          "(?i)(checksum64:).*"        = "`${1} $($Latest.Checksum64)"
+            "(?i)(\s+x32:).*"             = "`$1 $($Latest.URL32)"
+            "(?i)(checksum32:).*"         = "`$1 $($Latest.Checksum32)"
+            "(?i)(\s+x64:).*"             = "`$1 $($Latest.URL64)"
+            "(?i)(checksum64:).*"         = "`$1 $($Latest.Checksum64)"
         }
-  
     }
 }
 
-function global:au_BeforeUpdate { Get-RemoteFiles -Purge}
-
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge }
 
 function global:au_GetLatest {
-  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-  $latest_tag = (($download_page.BaseResponse).ResponseUri -split '\/')[-1]
-  $expanded_assets = Invoke-WebRequest "https://github.com/telegramdesktop/tdesktop/releases/expanded_assets/$($latest_tag)" -UseBasicParsing
+    # Holt Release-Infos via API
+    $release = Get-GitHubRelease 'telegramdesktop' 'tdesktop'
+    $assets = $release.assets
 
-  $re      = 'tsetup.*.exe'
-  $url64     = 'https://github.com' + ($expanded_assets.links | ? {($_.href -match $re) -and ($_.outerHTML -match 'Windows 64')} | select -First 1 -expand href)
-  $url32   = 'https://github.com' + ($expanded_assets.links | ? {($_.href -match $re) -and ($_.outerHTML -match 'Windows 32')} | select -First 1 -expand href)
-  $version = [regex]::match((($url32 -split '\/')[-1]),'[0-9]+(\.[0-9]+)*').value
-  
-  
+    # 64-Bit: Sucht nach 'tsetup-x64*.exe'
+    $url64 = $assets | Where-Object { $_.name -like 'tsetup-x64*.exe' } | Select-Object -First 1 -ExpandProperty browser_download_url
 
-     @{
-        URL64        = $url64
-        URL32        = $url32
-        Version      = $version
+    # 32-Bit: Sucht nach 'tsetup*.exe', schließt aber 'x64' aus
+    $url32 = $assets | Where-Object { $_.name -like 'tsetup*.exe' -and $_.name -notlike '*x64*' } | Select-Object -First 1 -ExpandProperty browser_download_url
+
+    # Entfernt führendes 'v' falls vorhanden
+    $version = $release.tag_name -replace '^v',''
+
+    @{
+        Version  = $version
+        URL64    = $url64
+        URL32    = $url32
+        FileType = 'exe'
     }
 }
-
 
 Update-Package -ChecksumFor none
